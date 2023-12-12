165f792a07e12f2373fbf7f1448fbddc7136ade3
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2023-12-12 17:30:58 -0400

    refactor: Move dsb config into separate file
    refactor: Make dsb config name explicit

---
+++ lib/src/digit_span_tasks/config_dsb.dart
@@ -0,0 +1,6 @@
+import 'package:digit_span_tasks/digit_span_tasks.dart';
+
+UserConfig configDSB = UserConfig(
+  stimListPractice: ['23', '567'],
+  stimListExperimental: ['0123', '45678', '901234'],
+);
--- lib/src/digit_span_tasks/run_dsb.dart
+++ lib/src/digit_span_tasks/run_dsb.dart
@@ -1,13 +1,11 @@
 import 'package:digit_span_tasks/digit_span_tasks.dart';
 import 'package:get/get.dart';
 
+import 'config_dsb.dart';
+
 void runDigitSpanBackwards() async {
-  UserConfig config = UserConfig(
-    stimListPractice: ['23', '567'],
-    stimListExperimental: ['0123', '45678', '901234'],
-  );
   DigitSpanTasksData data = await Get.to(() => DigitSpanBackwards(
-        config: config,
+        config: configDSB,
       ));
   // ignore: avoid_print
   print('\n\n\nBAKWARDS data \n $data');
