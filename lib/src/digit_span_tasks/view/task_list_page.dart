3c661fa90e14e47a423646b272689fd268fad694
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2023-12-12 19:01:43 -0400

    refactor: Make task list page stateless

--- lib/src/digit_span_tasks/view/task_list_page.dart
+++ lib/src/digit_span_tasks/view/task_list_page.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
 import 'task_buttons.dart';
 
-class TaskListPage extends StatefulWidget {
+class TaskListPage extends StatelessWidget {
   const TaskListPage({super.key});
 
-  @override
-  State<TaskListPage> createState() => _TaskListPageState();
-}
-
-class _TaskListPageState extends State<TaskListPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
