760e9d336f9b2b468152a7d6a2b26e0dc6208e15
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2023-12-15 12:47:03 -0400

    improve: Provide controller to participantID field

--- lib/src/participant_info/participant_info_dialog.dart
+++ lib/src/participant_info/participant_info_dialog.dart
@@ -1,10 +1,13 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import 'participant_id_textfield.dart';
 
 Future<void> showParticipantInfoDialog() async {
+  final TextEditingController controller = TextEditingController();
+
   await Get.defaultDialog(
     title: 'Información sobre sesión:',
-    content: const ParticipantIDTextInput(),
+    content: ParticipantIDTextInput(controller: controller),
   );
 }
