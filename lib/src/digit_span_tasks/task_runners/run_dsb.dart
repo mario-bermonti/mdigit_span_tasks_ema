9dca16ff3c82e1cde9f01ff4acd8717e2a20b9d4
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2023-12-15 12:40:27 -0400

    improve: Implement dialog for participant id

--- lib/src/digit_span_tasks/task_runners/run_dsb.dart
+++ lib/src/digit_span_tasks/task_runners/run_dsb.dart
@@ -1,8 +1,10 @@
 import 'package:digit_span_tasks/digit_span_tasks.dart';
 import 'package:get/get.dart';
+import '../../participant_info/participant_info_dialog.dart';
 import '../config/config_dsb.dart';
 
 void runDigitSpanBackwards() async {
+  await showParticipantInfoDialog();
   DigitSpanTasksData data = await Get.to(() => DigitSpanBackwards(
         config: configDSB,
       ));
--- lib/src/digit_span_tasks/task_runners/run_dsf.dart
+++ lib/src/digit_span_tasks/task_runners/run_dsf.dart
@@ -1,9 +1,11 @@
 import 'package:digit_span_tasks/digit_span_tasks.dart';
 import 'package:get/get.dart';
+import 'package:mdigit_span_tasks/src/participant_info/participant_info_dialog.dart';
 
 import '../config/config_dsf.dart';
 
 void runDigitSpanForward() async {
+  await showParticipantInfoDialog();
   DigitSpanTasksData data = await Get.to(() => DigitSpanForward(
         config: configDSF,
       ));
