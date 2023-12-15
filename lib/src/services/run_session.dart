39df56ac642a40d3db913e3b0283a449dc9a0f4e
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2023-12-15 15:03:48 -0400

    refactor: Rename start service to run session

--- lib/src/services/run_session.dart
+++ lib/src/services/run_session.dart
@@ -1,6 +1,6 @@
 /// Run a data collection session
 /// Running a session includes configuring everything needed and running a
 /// cognitive task specificed with [taskRunner].
-void startSession({required Function taskRunner}) {
+void runSession({required Function taskRunner}) {
   taskRunner();
 }
