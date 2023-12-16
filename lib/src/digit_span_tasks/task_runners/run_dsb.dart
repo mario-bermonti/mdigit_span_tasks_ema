5cf2ec4830cb2bce38fd4c2620f87a2409f0d6bb
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2023-12-16 16:32:03 -0400

    improve: Return data to caller after running dsb

--- lib/src/digit_span_tasks/task_runners/run_dsb.dart
+++ lib/src/digit_span_tasks/task_runners/run_dsb.dart
@@ -2,10 +2,13 @@ import 'package:digit_span_tasks/digit_span_tasks.dart';
 import 'package:get/get.dart';
 import '../config/config_dsb.dart';
 
-void runDigitSpanBackwards() async {
+Future<DigitSpanTasksData> runDigitSpanBackwards() async {
   DigitSpanTasksData data = await Get.to(() => DigitSpanBackwards(
         config: configDSB,
       ));
+
+  /// TODO remove when everything works
   // ignore: avoid_print
   print('\n\n\nBAKWARDS data \n $data');
+  return data;
 }
