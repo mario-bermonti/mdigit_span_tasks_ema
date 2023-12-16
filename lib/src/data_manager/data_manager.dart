b219b5102c270eb73440de4bdc2855d83afa29ed
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2023-12-16 16:54:03 -0400

    improve: Creating session id

--- lib/src/data_manager/data_manager.dart
+++ lib/src/data_manager/data_manager.dart
@@ -3,9 +3,16 @@ import 'package:digit_span_tasks/digit_span_tasks.dart';
 class DataManager {
   final String participantID;
   final DigitSpanTasksData data;
+  late final String sessionID;
 
   DataManager({
     required this.participantID,
     required this.data,
   });
+
+  void createSessionID() {
+    sessionID =
+        participantID + data.practiceData.sessionData.startTime.toString();
+    print(sessionID);
+  }
 }
