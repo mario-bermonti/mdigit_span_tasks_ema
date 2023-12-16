70b9d05dfb91ce25af88b692e08f09f7e345c36a
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2023-12-16 16:38:56 -0400

    improve: Require digit span data in data manager

--- lib/src/data_manager/data_manager.dart
+++ lib/src/data_manager/data_manager.dart
@@ -1,7 +1,11 @@
+import 'package:digit_span_tasks/digit_span_tasks.dart';
+
 class DataManager {
   final String participantID;
+  final DigitSpanTasksData data;
 
   DataManager({
     required this.participantID,
+    required this.data,
   });
 }
