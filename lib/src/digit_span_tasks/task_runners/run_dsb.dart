f20cdfa4bea8d536ee3ef52d89aa24ab362fd12b
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2023-12-16 13:42:04 -0400

    refactor: Run participant dialog in session runner

--- lib/src/digit_span_tasks/task_runners/run_dsb.dart
+++ lib/src/digit_span_tasks/task_runners/run_dsb.dart
@@ -1,10 +1,8 @@
 import 'package:digit_span_tasks/digit_span_tasks.dart';
 import 'package:get/get.dart';
-import '../../participant_info/participant_info_dialog.dart';
 import '../config/config_dsb.dart';
 
 void runDigitSpanBackwards() async {
-  await showParticipantInfoDialog();
   DigitSpanTasksData data = await Get.to(() => DigitSpanBackwards(
         config: configDSB,
       ));
