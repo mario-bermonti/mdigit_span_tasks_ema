4cd11ef86cbd6eb98cf20211fc4a2a9f47b13c2a
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2023-12-16 16:32:31 -0400

    improve: Return data to caller after running dsf

--- lib/src/digit_span_tasks/task_runners/run_dsf.dart
+++ lib/src/digit_span_tasks/task_runners/run_dsf.dart
@@ -2,10 +2,14 @@ import 'package:digit_span_tasks/digit_span_tasks.dart';
 import 'package:get/get.dart';
 import '../config/config_dsf.dart';
 
-void runDigitSpanForward() async {
+Future<DigitSpanTasksData> runDigitSpanForward() async {
   DigitSpanTasksData data = await Get.to(() => DigitSpanForward(
         config: configDSF,
       ));
+
+  /// TODO remove when everything works
   // ignore: avoid_print
   print('\n\n\nFORWARD data \n $data');
+
+  return data;
 }
