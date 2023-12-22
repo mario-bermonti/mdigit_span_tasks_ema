07b38069815e0dd02a5dbcc0a21cac117ac3ec95
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2023-12-22 17:14:41 -0400

    improve(data): Add db field

--- lib/src/data_manager/data_manager.dart
+++ lib/src/data_manager/data_manager.dart
@@ -1,8 +1,10 @@
+import 'package:cognitive_data/data.dart';
 import 'package:digit_span_tasks/digit_span_tasks.dart';
 
 class DataManager {
   final String participantID;
   final String sessionID;
+  late final DataBase _dataBase;
 
   /// TODO add type
   final data;
