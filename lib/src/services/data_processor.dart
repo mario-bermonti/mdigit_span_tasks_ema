ffe117947508777173d765414bdfdc0503c62ef1
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-01-11 10:20:20 -0400

    doc: Improve docstring processData

--- lib/src/services/data_processor.dart
+++ lib/src/services/data_processor.dart
@@ -2,7 +2,7 @@ import 'package:cognitive_data/data.dart';
 
 import '../data_manager/data_manager.dart';
 
-/// Process data from a single session
+/// Process data from a single session and save it to the db
 Future<void> processData({
   required String participantID,
   required String sessionID,
