442621c0d2075bbe6687bc9b7a37b493e3064816
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-01-16 09:36:38 -0400

    Improve: Define export button UI

---
+++ lib/src/export_data/view/export_button.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ExportButton extends StatelessWidget {
+  const ExportButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: const Icon(Icons.send_to_mobile),
+      onPressed: () {},
+    );
+  }
+}
