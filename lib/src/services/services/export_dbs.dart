5888b55add9057009cfa212eb1ee796eaaa1a03e
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-01-17 12:00:06 -0400

    doc: Improve documentation

--- lib/src/services/services/export_dbs.dart
+++ lib/src/services/services/export_dbs.dart
@@ -1,5 +1,5 @@
-/// Export all dbs
+/// Export dbs to make them accesible by users
 Future<void> exportDBs() async {}
 
-/// Export single db
+/// Export the db specified in [name]
 Future<void> _exportDB({required String name}) async {}
