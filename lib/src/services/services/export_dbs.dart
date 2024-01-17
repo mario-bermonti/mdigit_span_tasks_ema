93c7c7e95c433a51d10c9f3085e5e3286f12f758
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-01-17 12:52:31 -0400

    improve: Implement requestPermission

--- lib/src/services/services/export_dbs.dart
+++ lib/src/services/services/export_dbs.dart
@@ -1,5 +1,10 @@
+import 'package:mdigit_span_tasks/src/export_data/export_data/controller/controller.dart';
+
 /// Export dbs to make them accesible by users
 Future<void> exportDBs() async {}
 
 /// Export the db specified in [name]
-Future<void> _exportDB({required String name}) async {}
+Future<void> _exportDB({required String name}) async {
+  DataExporter dataExporter = DataExporter();
+  dataExporter.requestPermission();
+}
