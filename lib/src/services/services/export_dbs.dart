1b181c9002c89ff27719d5ea9d3103dc4fb42ad5
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-01-20 19:11:04 -0400

    fix: Await requestPermission

--- lib/src/services/services/export_dbs.dart
+++ lib/src/services/services/export_dbs.dart
@@ -9,7 +9,7 @@ Future<void> exportDBs() async {
 /// Export the db specified in [dbName]
 Future<void> _exportDB({required String dbName}) async {
   DataExporter dataExporter = DataExporter(dbName: dbName);
-  dataExporter.requestPermission();
+  await dataExporter.requestPermission();
   await dataExporter.getDB();
   await dataExporter.initDestinationFile();
   await dataExporter.copyDB();
