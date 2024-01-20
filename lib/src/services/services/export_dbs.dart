46d509d9348451c27a52c9a94af586012847d0de
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-01-20 18:51:07 -0400

    feature: Implement exportDBs

--- lib/src/services/services/export_dbs.dart
+++ lib/src/services/services/export_dbs.dart
@@ -1,7 +1,10 @@
 import 'package:mdigit_span_tasks/src/export_data/controller/data_exporter.dart';
 
 /// Export dbs to make them accesible by users
-Future<void> exportDBs() async {}
+Future<void> exportDBs() async {
+  await _exportDB(dbName: 'ds_forward.sqlite3');
+  await _exportDB(dbName: 'ds_backwards.sqlite3');
+}
 
 /// Export the db specified in [dbName]
 Future<void> _exportDB({required String dbName}) async {
