57f6cbb95ee4345941a302a88f964fb62c81053a
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-01-26 11:14:49 -0400

    improve(dsb): Add experimental trials

--- lib/src/digit_span_tasks/config/config_dsb.dart
+++ lib/src/digit_span_tasks/config/config_dsb.dart
@@ -1,9 +1,22 @@
 import 'package:digit_span_tasks/digit_span_tasks.dart';
 
 UserConfig configDSB = UserConfig(
-  stimListExperimental: ['0123', '45678', '901234'],
   stimListPractice: [
     '36',
     '283',
+  ],
+  stimListExperimental: [
+    '26',
+    '49',
+    '926',
+    '612',
+    '6157',
+    '3261',
+    '18542',
+    '24781',
+    '483659',
+    '917468',
+    '3692478',
+    '3751682',
   ],
 );
