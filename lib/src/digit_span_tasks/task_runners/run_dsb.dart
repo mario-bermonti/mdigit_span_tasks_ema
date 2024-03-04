cbee956042add5599bc8f47a6da1df807bcb6653
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-03-04 08:53:11 -0400

    refactor: Require participant/sessionID in dsb

--- lib/src/digit_span_tasks/task_runners/run_dsb.dart
+++ lib/src/digit_span_tasks/task_runners/run_dsb.dart
@@ -2,7 +2,10 @@ import 'package:digit_span_tasks/digit_span_tasks.dart';
 import 'package:get/get.dart';
 import '../config/config_dsb.dart';
 
-Future<DigitSpanTasksData> runDigitSpanBackwards() async {
+Future<DigitSpanTasksData> runDigitSpanBackwards({
+  required String participantID,
+  required String sessionID,
+}) async {
   DigitSpanTasksData data = await Get.to(() => DigitSpanBackwards(
         config: configDSB,
       ));
