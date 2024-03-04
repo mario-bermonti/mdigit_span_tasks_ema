cc704e3929a6054dd26853a0b1f62c92645ed5bd
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-03-04 09:40:55 -0400

    improve: Use new DSFConfig in dsf runner
    improve: Use new DSFConfig in dsf runner
    
    fix: Fix userConfig name in runDigitSpanForward
    
    Can be squashed into previous commits

--- lib/src/digit_span_tasks/task_runners/run_dsf.dart
+++ lib/src/digit_span_tasks/task_runners/run_dsf.dart
@@ -6,8 +6,15 @@ Future<DigitSpanTasksData> runDigitSpanForward({
   required String participantID,
   required String sessionID,
 }) async {
+  final DSFConfig dsfConfig = DSFConfig();
+  final UserConfig userConfig = UserConfig(
+      stimListPractice: dsfConfig.practiceStim,
+      stimListExperimental: dsfConfig.experimentalStim,
+      participantID: participantID,
+      sessionID: sessionID);
+
   DigitSpanTasksData data = await Get.to(() => DigitSpanForward(
-        config: configDSF,
+        config: userConfig,
       ));
 
   return data;
