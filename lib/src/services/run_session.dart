d20f946168dc8ed32d55497a65f57aa0ac834feb
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-03-05 18:00:27 -0400

    maint: Fix typo

--- lib/src/services/run_session.dart
+++ lib/src/services/run_session.dart
@@ -7,7 +7,7 @@ import '../participant_info/participant_info_dialog.dart';
 
 /// Run a data collection session
 /// Running a session includes configuring everything needed and running a
-/// cognitive task specificed with [taskRunner].
+/// cognitive task specified with [taskRunner].
 void runSession(
     {required Function({
       required String participantID,
