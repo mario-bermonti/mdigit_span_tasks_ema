74e7f212537421d17a535dd750ab6a100a94ad4f
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-03-05 09:30:18 -0400

    doc: Update docstring processData()

--- lib/src/services/data_processor.dart
+++ lib/src/services/data_processor.dart
@@ -2,7 +2,7 @@ import 'package:digit_span_tasks/digit_span_tasks.dart';
 
 import '../data_manager/data_manager.dart';
 
-/// Process data from a single session and save it to the db
+/// Process data from a cognitive task's single session and save it to the db
 Future<void> processData({
   required String dbName,
   required DigitSpanTaskData data,
