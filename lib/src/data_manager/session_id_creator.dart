28770c93ad48c47023dd76f1bc1f6039e41f71c6
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-03-06 09:28:45 -0400

    doc: Add docstring to createSessionID()

--- lib/src/data_manager/session_id_creator.dart
+++ lib/src/data_manager/session_id_creator.dart
@@ -1,3 +1,5 @@
+/// Create a unique [String] identifier by concatenating the [participantID]
+/// with the current time.
 String createSessionID({required String participantID}) {
   final DateTime startTime = DateTime.now();
   final String sessionID = "${participantID}_${startTime.toString()}";
