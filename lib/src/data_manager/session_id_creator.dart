6433e5b87401e352876fbd8629e319de95f31c2e
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-03-06 09:30:08 -0400

    improve: Improve formatting of sessionID

--- lib/src/data_manager/session_id_creator.dart
+++ lib/src/data_manager/session_id_creator.dart
@@ -1,6 +1,6 @@
 String createSessionID({required String participantID}) {
   final DateTime startTime = DateTime.now();
-  final String sessionID = participantID + startTime.toString();
+  final String sessionID = "${participantID}_${startTime.toString()}";
 
   return sessionID;
 }
