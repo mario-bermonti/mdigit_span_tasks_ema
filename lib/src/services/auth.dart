40e5d87118f85d253c4a91bc6b2080e78740f86f
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-04-05 06:25:24 -0400

    improve: Add method for registering participants
    improve: Add method for registering participants
    
    refactor: Rename user credentials
    
    refactor: Sign in participants anon

--- lib/src/services/auth.dart
+++ lib/src/services/auth.dart
@@ -2,4 +2,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 class Auth {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<void> signIn() async {
+    final UserCredential userCredential = await _auth.signInAnonymously();
+  }
 }
