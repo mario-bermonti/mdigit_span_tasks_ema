0087eccd6510ecb9b77caccd8af423b7cdb465fa
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-04-05 08:37:41 -0400

    improve: Use anon sign-in

--- lib/src/services/run_session.dart
+++ lib/src/services/run_session.dart
@@ -1,6 +1,7 @@
 import 'package:digit_span_tasks/digit_span_tasks.dart';
 import 'package:flutter/material.dart';
 import 'package:mdigit_span_tasks_ema/src/data_manager/session_id_creator.dart';
+import 'package:mdigit_span_tasks_ema/src/services/auth.dart';
 import 'package:mdigit_span_tasks_ema/src/services/data_processor.dart';
 
 import '../participant_info/participant_info_dialog.dart';
@@ -26,6 +27,8 @@ void runSession(
     participantID: participantID,
     sessionID: sessionID,
   );
+
+  await Auth().signIn();
 
   await processData(
     participantID: participantID,
