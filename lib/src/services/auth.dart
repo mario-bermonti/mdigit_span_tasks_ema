8b162f2cc91ad967849029373012401df9af61e4
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-04-05 11:44:05 -0400

    refactor: Make Auth._auth public

--- lib/src/services/auth.dart
+++ lib/src/services/auth.dart
@@ -2,10 +2,10 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 /// Manages authorization processes of the app
 class Auth {
-  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseAuth auth = FirebaseAuth.instance;
 
   /// Add or sign in the user anonymously into firebase.
   Future<void> signIn() async {
-    final UserCredential userCredential = await _auth.signInAnonymously();
+    final UserCredential userCredential = await auth.signInAnonymously();
   }
 }
