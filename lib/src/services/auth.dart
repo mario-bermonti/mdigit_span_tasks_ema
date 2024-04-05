53c82de159edf7dff42aaf3b771852732bff34c9
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-04-05 19:16:06 -0400

    doc: Update Auth docstrings

--- lib/src/services/auth.dart
+++ lib/src/services/auth.dart
@@ -1,13 +1,13 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
-/// Manages authorization processes of the app
+/// Manages authorization processes for the app
 class Auth {
-  /// [FirebaseAuth] instance used to access auth functionality
+  /// [FirebaseAuth] instance used to access auth functionality.
   final FirebaseAuth auth;
 
   Auth({required this.auth});
 
-  /// Add or sign in the user anonymously into firebase.
+  ///  Anonymously add or sign in the user into firebase.
   Future<void> signIn() async {
     final UserCredential userCredential = await auth.signInAnonymously();
   }
