2ade337e9d838e0e86c4adfdfe810130143db12b
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-04-05 20:47:09 -0400

    improve: Add named constructor to Participant
    improve: Add named constructor to User model
    
    fix: Fix intentional bug for testing

--- lib/src/auth/user_model.dart
+++ lib/src/auth/user_model.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
 class Participant {
-  final String uid;
-  final DateTime registerDateTime;
+  final String? uid;
+  final DateTime? registerDateTime;
 
   Participant({
     required this.uid,
     required this.registerDateTime,
   });
+
+  Participant.fromUserCredential(UserCredential userCredential)
+      : uid = userCredential.user?.uid,
+        registerDateTime = userCredential.user?.metadata.creationTime;
 }
