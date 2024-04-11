d541e64730d5fea6ea16c20c034ddb9ae0cf56c2
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-04-11 19:01:48 -0400

    improve: Define the Notifications._notifications

--- lib/src/notifications/notifications.dart
+++ lib/src/notifications/notifications.dart
@@ -1,4 +1,9 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:get/get.dart';
 
-class Notifications extends GetxController {}
+class Notifications extends GetxController {
+  final FlutterLocalNotificationsPlugin _notifications =
+      FlutterLocalNotificationsPlugin();
+}
