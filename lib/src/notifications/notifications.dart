8f46752a1e1c77b3a31061c939c3d48ab0034271
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-04-15 09:05:46 -0400

    maint: Clean imports in notifications

--- lib/src/notifications/notifications.dart
+++ lib/src/notifications/notifications.dart
@@ -1,7 +1,4 @@
 import 'dart:io';
-
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:get/get.dart';
 
