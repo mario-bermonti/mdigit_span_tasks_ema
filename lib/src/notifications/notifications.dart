9e21671a7f398eb8c046fd076a546bde9fa063ad
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-04-15 09:46:46 -0400

    doc: Add docstrings to Notifications

--- lib/src/notifications/notifications.dart
+++ lib/src/notifications/notifications.dart
@@ -2,6 +2,9 @@ import 'dart:io';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:get/get.dart';
 
+/// Manager for local notifications
+///
+/// It is a [GetXController] so it can be accessed from anywhere in the app.
 class LocalNotifications extends GetxController {
   final FlutterLocalNotificationsPlugin _notifications =
       FlutterLocalNotificationsPlugin();
@@ -13,6 +16,9 @@ class LocalNotifications extends GetxController {
     await init();
   }
 
+  /// Initializes the [LocalNotifications] for android and ios.
+  ///
+  /// Must be called before using the notifications objects.
   Future<void> init() async {
     const AndroidInitializationSettings initializationSettingsAndroid =
         AndroidInitializationSettings('@mipmap/ic_launcher');
@@ -26,6 +32,10 @@ class LocalNotifications extends GetxController {
     await _notifications.initialize(initializationSettings);
   }
 
+  /// Asks the user during runtime for permission to send notifications.
+  ///
+  /// Only android is supported. Will cause throw an [Exception] if the
+  /// platform is not supported or the user does not grant permission.
   Future<void> askNotificationPermission() async {
     if (!Platform.isAndroid) {
       throw Exception("Notifications not implemented for this platform");
@@ -43,6 +53,7 @@ class LocalNotifications extends GetxController {
     }
   }
 
+  /// Configures the details for the supported platforms and returns them.
   NotificationDetails get notificationDetails {
     const AndroidNotificationDetails androidNotificationDetails =
         AndroidNotificationDetails(
@@ -62,6 +73,7 @@ class LocalNotifications extends GetxController {
     return notificationDetails;
   }
 
+  /// Shows a local notification to the user with the given information.
   Future<void> showNotification({
     int id = 0,
     required String title,
