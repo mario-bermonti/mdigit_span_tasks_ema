a5074ad08dbbdc4f671f1b76cbac6ab2e3e5d2ce
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-04-15 18:55:19 -0400

    feat: Define FirebaseNotifications._notifications
    feat: Define FirebaseNotifications._notifications
    
    refactor: Make FCM instance public
    
    It is accessible anyway by just FirebaseMessaging.instance

--- lib/src/notifications/firebase_notifications.dart
+++ lib/src/notifications/firebase_notifications.dart
@@ -1,3 +1,6 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:get/get.dart';
 
-class FirebaseNotifications extends GetxController {}
+class FirebaseNotifications extends GetxController {
+  final FirebaseMessaging notifications = FirebaseMessaging.instance;
+}
