cd1bedd8d94f78e52c4fbf8f7b9a8a0cde8d222b
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-04-15 18:51:14 -0400

    refactor: Rename to local_notifications
    refactor: Rename filename to local_notifications
    
    refactor: Rename LocalNotifications import
    
    Can be squashed into previous commits

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:mdigit_span_tasks_ema/src/app.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:mdigit_span_tasks_ema/src/notifications/notifications.dart';
+import 'package:mdigit_span_tasks_ema/src/notifications/local_notifications.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
