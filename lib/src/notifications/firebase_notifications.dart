b77af941323c1f86ce81c14f53071412d9a20142
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-04-16 14:15:28 -0400

    improve: Declare handler for background messages

--- lib/src/notifications/firebase_notifications.dart
+++ lib/src/notifications/firebase_notifications.dart
@@ -1,6 +1,9 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:get/get.dart';
 
+Future<void> handleBackgroundMessages(RemoteMessage message) async {
+}
+
 class FirebaseNotifications extends GetxController {
   final FirebaseMessaging notifications = FirebaseMessaging.instance;
 
