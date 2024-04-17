db1822cb27935cd3e2faa2a47efd561a75899bdb
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-04-17 08:34:00 -0400

    doc: Add docstring to FirebaseNotifications

--- lib/src/notifications/firebase_notifications.dart
+++ lib/src/notifications/firebase_notifications.dart
@@ -1,9 +1,15 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:get/get.dart';
 
+/// Handles messages received from FCM while in the background.
+///
+/// It needs to be a top-level method (static method do not work).
 Future<void> handleBackgroundMessages(RemoteMessage message) async {
 }
 
+/// Service that handles all interactions with FCM.
+///
+/// It is a [GetxController] so it can be accessed from anywhere in the app.
 class FirebaseNotifications extends GetxController {
   final FirebaseMessaging notifications = FirebaseMessaging.instance;
 
@@ -13,6 +19,7 @@ class FirebaseNotifications extends GetxController {
     await init();
   }
 
+  /// Initializes the FCM and all necessary configuration.
   Future<void> init() async {
     await notifications.requestPermission();
     final String? token = await notifications.getToken();
