518cf23353e3d85d19cf96f863120a0c40e5a346
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-04-25 12:22:38 -0400

    feature: Randomize EMA task order

--- lib/src/ema/run_ema_tasks.dart
+++ lib/src/ema/run_ema_tasks.dart
@@ -4,8 +4,20 @@ import 'package:mdigit_span_tasks_ema/src/digit_span_tasks/task_runners/run_dsb.
 import '../digit_span_tasks/task_runners/run_dsf.dart';
 import '../services/run_session.dart';
 
-Future<void> runEMATasks() async {
+void runEMAdsf() async {
   await runSession(taskRunner: runDigitSpanForward, taskName: 'ds_forward');
+}
+
+void runEMAdsb() async {
   await runSession(taskRunner: runDigitSpanBackwards, taskName: 'ds_backwards');
+}
+
+final List<Function> emaTasks = <Function>[runEMAdsf, runEMAdsb];
+
+Future<void> runEMATasks() async {
+  emaTasks.shuffle();
+  for (Function emaTask in emaTasks) {
+    await emaTask();
+  }
   Get.back();
 }
