630e2572a230e8861b803525f61a2c27a3d294ca
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-05-02 10:10:13 -0400

    refactor: Remove old app bar impl

--- lib/src/app_bar/app_bar.dart
+++ lib/src/app_bar/app_bar.dart
@@ -2,12 +2,6 @@ import 'package:flutter/material.dart';
 
 import '../notifications/notifications_button.dart';
 
-final AppBar appBar = AppBar(
-  title: const Text('mDigitSpanTask'),
-  centerTitle: true,
-  actions: [NotificationsButton()],
-);
-
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   final bool allowBack;
 
