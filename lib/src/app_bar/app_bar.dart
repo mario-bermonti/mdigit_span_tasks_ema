96ea1524927afdd8936d58d21ec44b3b65b3fc4e
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-05-02 10:07:48 -0400

    improve: Impl custom app bar

--- lib/src/app_bar/app_bar.dart
+++ lib/src/app_bar/app_bar.dart
@@ -7,3 +7,22 @@ final AppBar appBar = AppBar(
   centerTitle: true,
   actions: [NotificationsButton()],
 );
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final bool allowBack;
+
+  const CustomAppBar({super.key, this.allowBack = false});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: const Text('mDigitSpanTask'),
+      centerTitle: true,
+      automaticallyImplyLeading: allowBack,
+      actions: [NotificationsButton()],
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
