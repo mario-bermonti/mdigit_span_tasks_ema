aa1d529fc035726e86dcdd04b0d1c7dc7553e903
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-05-20 08:12:33 -0400

    refactor: Adapt dsb to new impl
    refactor: Adapt practice session dsb
    
    refactor: Adapt dsb to new impl
    
    refactor: Fix DSB config name
    
    Fix: Rename dsb practice data
    
    refactor: Adapt dsb experimental trials

--- lib/src/digit_span_tasks/task_runners/run_dsb.dart
+++ lib/src/digit_span_tasks/task_runners/run_dsb.dart
@@ -1,23 +1,43 @@
 import 'package:digit_span_tasks/digit_span_tasks.dart';
 import 'package:get/get.dart';
 import '../config/config_dsb.dart';
+import '../prep_data.dart';
 
 /// Configure and start the DSB
 Future<DigitSpanTaskData> runDigitSpanBackwards({
   required String participantID,
   required String sessionID,
 }) async {
+  DigitSpanTask task;
   final DSBConfig dsbConfig = DSBConfig();
-  final UserConfig userConfig = UserConfig(
-    stimListPractice: dsbConfig.practiceStim,
-    stimListExperimental: dsbConfig.experimentalStim,
+  final UserConfig userConfigPractice = UserConfig(
+    stimList: dsbConfig.practiceStim,
     participantID: participantID,
     sessionID: sessionID,
+    sessionType: SessionType.practice,
   );
 
-  DigitSpanTaskData data = await Get.to(() => DigitSpanBackwards(
-        config: userConfig,
-      ));
+  task = DigitSpanTask(
+    config: userConfigPractice,
+  );
+  DigitSpanTaskData practiceData = await task.run();
+
+  final UserConfig userConfigExperimental = UserConfig(
+    stimList: dsbConfig.experimentalStim,
+    participantID: participantID,
+    sessionID: sessionID,
+    sessionType: SessionType.experimental,
+  );
+
+  task = DigitSpanTask(
+    config: userConfigExperimental,
+  );
+  DigitSpanTaskData experimentalData = await task.run();
+
+  final DigitSpanTaskData data = prepData(
+    practiceData: practiceData,
+    experimentalData: experimentalData,
+  );
 
   return data;
 }
