58f1fa8d6bc57b5c1f5b26f0382d199a34c2376e
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-05-20 08:12:08 -0400

    refactor: Adapt dsf to new impl
    refactor: Adapt practice session dsb
    
    refactor: Adapt dsb to new impl
    
    refactor: Fix DSB config name
    
    Fix: Rename dsb practice data
    
    refactor: Adapt dsb experimental trials
    
    refactor: Remove prep_data function from dsf

--- lib/src/digit_span_tasks/task_runners/run_dsf.dart
+++ lib/src/digit_span_tasks/task_runners/run_dsf.dart
@@ -1,22 +1,43 @@
 import 'package:digit_span_tasks/digit_span_tasks.dart';
 import 'package:get/get.dart';
 import '../config/config_dsf.dart';
+import '../prep_data.dart';
 
 /// Configure and start the DSF
 Future<DigitSpanTaskData> runDigitSpanForward({
   required String participantID,
   required String sessionID,
 }) async {
+  DigitSpanTask task;
   final DSFConfig dsfConfig = DSFConfig();
-  final UserConfig userConfig = UserConfig(
-      stimListPractice: dsfConfig.practiceStim,
-      stimListExperimental: dsfConfig.experimentalStim,
-      participantID: participantID,
-      sessionID: sessionID);
+  final UserConfig userConfigPractice = UserConfig(
+    stimList: dsfConfig.practiceStim,
+    participantID: participantID,
+    sessionID: sessionID,
+    sessionType: SessionType.practice,
+  );
 
-  DigitSpanTaskData data = await Get.to(() => DigitSpanForward(
-        config: userConfig,
-      ));
+  task = DigitSpanTask(
+    config: userConfigPractice,
+  );
+  DigitSpanTaskData practiceData = await task.run();
+
+  final UserConfig userConfigExperimental = UserConfig(
+    stimList: dsfConfig.experimentalStim,
+    participantID: participantID,
+    sessionID: sessionID,
+    sessionType: SessionType.experimental,
+  );
+
+  task = DigitSpanTask(
+    config: userConfigExperimental,
+  );
+  DigitSpanTaskData experimentalData = await task.run();
+
+  final DigitSpanTaskData data = prepData(
+    practiceData: practiceData,
+    experimentalData: experimentalData,
+  );
 
   return data;
 }
