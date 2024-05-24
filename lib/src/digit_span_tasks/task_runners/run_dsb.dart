0290cc531088de860c3730e106eee254c7a852b3
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-05-24 17:30:41 -0400

    improve: Add instructions to dsb

--- lib/src/digit_span_tasks/task_runners/run_dsb.dart
+++ lib/src/digit_span_tasks/task_runners/run_dsb.dart
@@ -1,4 +1,8 @@
 import 'package:digit_span_tasks/digit_span_tasks.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:mdigit_span_tasks_ema/src/ui_components/loading_screen.dart';
+import '../../ui_components/instructions.dart';
 import '../config/config_dsb.dart';
 import '../prep_data.dart';
 
@@ -7,6 +11,35 @@ Future<DigitSpanTaskData> runDigitSpanBackwards({
   required String participantID,
   required String sessionID,
 }) async {
+  Get.to(() => const LoadingScreen());
+  await Get.to(
+    () => const Instructions(
+        instructions:
+            Text('Recuerda los números al revés al orden en que los veas')),
+  );
+  await Get.to(
+    () => const Instructions(
+        instructions: Column(
+      children: [
+        Text('Ejemplo:'),
+        Text('Si ves 49'),
+        Text('Escribe 94'),
+      ],
+    )),
+  );
+  await Get.to(
+    () => const Instructions(
+        instructions: Column(
+      children: [
+        Text('Ejemplo:'),
+        Text('Si ves 491'),
+        Text('Escribe 194'),
+      ],
+    )),
+  );
+  await Get.to(
+    () => const Instructions(instructions: Text('Comencemos practicando')),
+  );
   DigitSpanTask task;
   final DSBConfig dsbConfig = DSBConfig();
   final UserConfig userConfigPractice = UserConfig(
@@ -20,6 +53,17 @@ Future<DigitSpanTaskData> runDigitSpanBackwards({
     config: userConfigPractice,
   );
   DigitSpanTaskData practiceData = await task.run();
+
+  await Get.to(
+    () => const Instructions(
+        instructions:
+            Text('Ahora trabajaremos con los ejercicios principales')),
+  );
+  await Get.to(
+    () => const Instructions(
+        instructions: Text(
+            'Recuerda escribir los números al revés al orden en que los veas')),
+  );
 
   final UserConfig userConfigExperimental = UserConfig(
     stimList: dsbConfig.experimentalStim,
@@ -38,5 +82,9 @@ Future<DigitSpanTaskData> runDigitSpanBackwards({
     experimentalData: experimentalData,
   );
 
+  await Get.to(() =>
+      const Instructions(instructions: Text('¡Terminamos esta actividad!')));
+
+  Get.toNamed('/');
   return data;
 }
