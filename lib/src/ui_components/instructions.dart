3ac6d4e8829b27694d8568d38b15d6f2ef6c0bce
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-05-24 16:37:20 -0400

    improve: Improve instructions widget
    improve: Improve instructions widget
    
    improve: Make instructions accept a widget

--- lib/src/ui_components/instructions.dart
+++ lib/src/ui_components/instructions.dart
@@ -1,19 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:mdigit_span_tasks_ema/src/ui_components/screen.dart';
 
 class Instructions extends StatelessWidget {
-  final String text;
+  final Widget instructions;
 
-  const Instructions({super.key, required this.text});
+  const Instructions({super.key, required this.instructions});
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () => Get.back(),
-      child: Text(
-        text,
-        style: Theme.of(context).textTheme.titleLarge,
-        textAlign: TextAlign.center,
+    return Screen(
+      children: Column(
+        children: <Widget>[
+          instructions,
+          ElevatedButton(
+            onPressed: () => Get.back(),
+            child: Text(
+              'Continuar',
+              style: Theme.of(context).textTheme.titleLarge,
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ],
       ),
     );
   }
