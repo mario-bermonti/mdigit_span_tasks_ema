1810b2133f25a87e7cc9b66e044ed953b601490c
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-05-31 18:58:56 -0400

    improve: Define task config as controller
    improve: Define task config as controller
    
    improve: Define task config as controller
    
    improve: Add stim to task config
    
    refactor: Remove next screen from config

---
+++ lib/src/digit_span_tasks/config/config.dart
@@ -0,0 +1,9 @@
+import 'package:get/get.dart';
+
+class DigitSpanTaskConfig extends GetxController {
+  late String participantID;
+  late String sessionID;
+  late String taskName;
+  late List<String> practiceStim;
+  late List<String> experimentalStim;
+}
