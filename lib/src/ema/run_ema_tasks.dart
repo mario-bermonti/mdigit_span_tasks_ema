f44d6f20e47f051ae99ffca3b855e59b73964109
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-06-02 14:28:07 -0400

    improve: Use new config in ema tasks

--- lib/src/ema/run_ema_tasks.dart
+++ lib/src/ema/run_ema_tasks.dart
@@ -1,15 +1,28 @@
 import 'package:get/get.dart';
 import 'package:mdigit_span_tasks_ema/src/digit_span_tasks/task_runners/run_dsb.dart';
 
+import '../digit_span_tasks/config/config.dart';
+import '../digit_span_tasks/config/stim_dsb.dart';
+import '../digit_span_tasks/config/stim_dsf.dart';
 import '../digit_span_tasks/task_runners/run_dsf.dart';
 import '../services/run_session.dart';
 
 void runEMAdsf() async {
-  await runSession(taskRunner: runDigitSpanForward, taskName: 'ds_forward');
+  final DigitSpanTaskConfig config = Get.find();
+  config.taskName = 'dsf';
+  final DSFStim stim = DSFStim();
+  config.practiceStim = stim.practiceStim;
+  config.experimentalStim = stim.experimentalStim;
+  await runSession(taskRunner: runDigitSpanForward);
 }
 
 void runEMAdsb() async {
-  await runSession(taskRunner: runDigitSpanBackwards, taskName: 'ds_backwards');
+  final DigitSpanTaskConfig config = Get.find();
+  config.taskName = 'dsb';
+  final DSBStim stim = DSBStim();
+  config.practiceStim = stim.practiceStim;
+  config.experimentalStim = stim.experimentalStim;
+  await runSession(taskRunner: runDigitSpanBackwards);
 }
 
 final List<Function> emaTasks = <Function>[runEMAdsf, runEMAdsb];
