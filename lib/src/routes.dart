0d7fb4836dc5d59480b49b0cbd3661f13596be6e
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-06-04 10:24:54 -0400

    improve: Add loading screen to named routes

--- lib/src/routes.dart
+++ lib/src/routes.dart
@@ -1,8 +1,10 @@
 import 'package:get/get.dart';
 import 'package:mdigit_span_tasks_ema/src/ema/ema_screen.dart';
 import 'package:mdigit_span_tasks_ema/src/task_list/view/task_list_page.dart';
+import 'package:mdigit_span_tasks_ema/src/ui_components/loading_screen.dart';
 
 final List<GetPage> routes = <GetPage>[
   GetPage(name: '/', page: () => const TaskListPage()),
   GetPage(name: '/emaScreen', page: () => const EMAScreen()),
+  GetPage(name: '/loading', page: () => const LoadingScreen()),
 ];
