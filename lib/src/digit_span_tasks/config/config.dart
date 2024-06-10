f22971e369129c9d249dfa156e1d1e2078b86913
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-06-10 10:19:37 -0400

    improve: Require stim specs in config

--- lib/src/digit_span_tasks/config/config.dart
+++ lib/src/digit_span_tasks/config/config.dart
@@ -1,10 +1,14 @@
 import 'package:get/get.dart';
-import 'package:mdigit_span_tasks_ema/src/digit_span_tasks/config/ds_stim.dart';
 
 class DigitSpanTaskConfig extends GetxController {
   late String participantID;
   late String sessionID;
   late String taskName;
-  late DSStim stim;
+  late int practiceMinStimSize;
+  late int practiceMaxStimSize;
+  late int practiceCountEachSize;
+  late int experimentalMinStimSize;
+  late int experimentalMaxStimSize;
+  late int experimentalCountEachSize;
   late String nextScreen;
 }
