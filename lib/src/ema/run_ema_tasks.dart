93da19907605fea1d0bd108e564de7fd0023968e
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-06-10 10:26:49 -0400

    refactor: Adapt dsf stim

--- lib/src/ema/run_ema_tasks.dart
+++ lib/src/ema/run_ema_tasks.dart
@@ -2,16 +2,18 @@ import 'package:get/get.dart';
 import 'package:mdigit_span_tasks_ema/src/digit_span_tasks/task_runners/run_dsb.dart';
 
 import '../digit_span_tasks/config/config.dart';
-import '../digit_span_tasks/config/ds_stim.dart';
-import '../digit_span_tasks/config/stim_dsb.dart';
-import '../digit_span_tasks/config/stim_dsf.dart';
 import '../digit_span_tasks/task_runners/run_dsf.dart';
 import '../services/run_session.dart';
 
 void runEMAdsf() async {
   final DigitSpanTaskConfig config = Get.find();
   config.taskName = 'dsf';
-  config.stim = dsfStim;
+  config.practiceMinStimSize = 2;
+  config.practiceMaxStimSize = 2;
+  config.practiceCountEachSize = 1;
+  config.experimentalMinStimSize = 4;
+  config.experimentalMaxStimSize = 4;
+  config.experimentalCountEachSize = 1;
   await runSession(taskRunner: runDigitSpanForward);
 }
 
