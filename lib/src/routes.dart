e5a0c3d59f7df34d8a3db64e857b95d414683d58
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-06-19 11:23:44 -0400

    feat: Impl baseline survey screen
    feat: Impl basic baseline survey
    
    feat: Impl baseline survey screen
    
    feat: Impl baseline survey screen
    
    feat: Add baseline survey route as home
    
    feat: Add baseline survey route as home
    
    fix: Fix import of baseline survey screen
    
    improve: Use new impl of building questions
    
    improve: Use controller to present survey screen
    
    improve: Use controller when building survey
    
    refactor: Use controller's sexQuestion in survey
    
    refactor: Rename baseline controller in survey
    
    improve: Load baseline controller in view
    
    improve: Build baseline survey ui using controller
    
    fix: Don't mark Baseline route as const
    
    maint: Delete survey.dart
    
    maint: Delete survey.dart
    
    maint: Clean imports

--- lib/src/routes.dart
+++ lib/src/routes.dart
@@ -3,8 +3,11 @@ import 'package:mdigit_span_tasks_ema/src/ema/ema_screen.dart';
 import 'package:mdigit_span_tasks_ema/src/task_list/view/task_list_page.dart';
 import 'package:mdigit_span_tasks_ema/src/ui_components/loading_screen.dart';
 
+import 'baseline/baseline_survey_screen.dart';
+
 final List<GetPage> routes = <GetPage>[
-  GetPage(name: '/', page: () => const TaskListPage()),
+  GetPage(name: '/', page: () => BaselineSurveyScreen()),
+  GetPage(name: '/tasklist', page: () => const TaskListPage()),
   GetPage(name: '/emaScreen', page: () => const EMAScreen()),
   GetPage(name: '/loading', page: () => const LoadingScreen()),
 ];
