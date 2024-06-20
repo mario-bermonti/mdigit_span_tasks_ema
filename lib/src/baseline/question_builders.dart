2560b28c8a5538c5825bd97e6272770347197b92
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-06-20 10:45:23 -0400

    improve: Impl builder for single choice questions
    improve: Impl builder for single choice questions
    
    improve: Impl buildSingleChoiceQuestion
    
    test: Add test for buildSingleChoiceQuestion
    
    test: Add test for buildSingleChoiceQuestion
    
    test: Add test for buildSingleChoiceQuestion
    
    INCOMPLETE TEST buildSingleChoiceQuestion
    
    test: Add test for buildSingleChoiceQuestion
    
    refactor(test): Clean code building question
    
    test: Clean import question builder test
    
    test: Change example in question builder test
    
    doc: Update buildSingleChoiceQuestion docstring

--- lib/src/baseline/question_builders.dart
+++ lib/src/baseline/question_builders.dart
@@ -1,5 +1,25 @@
 import 'package:research_package/research_package.dart';
 
+/// Build a [reserach_package] single choice question from a json object.
+/// Build a [research_package] single choice question from a json object.
+/// The question build is of type [RPChoiceAnswerStyle.SingleChoice] from the
+/// research package.
+RPQuestionStep buildSingleChoiceQuestion({
+  required Map<String, dynamic> config,
+}) {
+  final List<RPChoice> choices = buildChoices(choices: config['choices']);
+  final RPChoiceAnswerFormat answerFormat = RPChoiceAnswerFormat(
+    answerStyle: RPChoiceAnswerStyle.SingleChoice,
+    choices: choices,
+  );
+  final RPQuestionStep question = RPQuestionStep(
+    identifier: config['identifier'],
+    title: config['title'],
+    answerFormat: answerFormat,
+  );
+  return question;
+}
+
 /// Build [RPChoice] objects from a json object.
 List<RPChoice> buildChoices({required List<Map<String, dynamic>> choices}) {
   final List<RPChoice> formattedChoices = choices
--- test/src/baseline/question_builders_test.dart
+++ test/src/baseline/question_builders_test.dart
@@ -4,6 +4,57 @@ import 'package:research_package/model.dart';
 
 void main() {
   test(
+    "Given a map with identifier, title, and choices, returns an RPQuestionStep",
+    () {
+      final List<RPChoice> choicesExpected = <RPChoice>[
+        RPChoice(text: 'black', value: 0),
+        RPChoice(text: 'white', value: 1),
+      ];
+
+      final RPChoiceAnswerFormat answerFormat = RPChoiceAnswerFormat(
+        answerStyle: RPChoiceAnswerStyle.SingleChoice,
+        choices: choicesExpected,
+      );
+
+      final RPQuestionStep questionExpected = RPQuestionStep(
+        identifier: 'color',
+        title: 'color?',
+        answerFormat: answerFormat,
+      );
+
+      final Map<String, dynamic> questionJson = {
+        'identifier': questionExpected.identifier,
+        'title': questionExpected.title,
+        'choices': [
+          {
+            'text': choicesExpected.first.text,
+            'value': choicesExpected.first.value,
+          },
+          {
+            'text': choicesExpected.last.text,
+            'value': choicesExpected.last.value,
+          }
+        ]
+      };
+
+      final RPQuestionStep questionActual =
+          buildSingleChoiceQuestion(config: questionJson);
+
+      expect(questionActual.identifier, questionExpected.identifier);
+      expect(questionActual.title, questionExpected.title);
+      final List<RPChoice> choicesActual =
+          questionActual.answerFormat.toJson()['choices'];
+      expect(
+        choicesActual.first.toJson(),
+        choicesExpected.first.toJson(),
+      );
+      expect(
+        choicesActual.last.toJson(),
+        choicesExpected.last.toJson(),
+      );
+    },
+  );
+  test(
     "Given a list of maps that identify a label and value, buildChoices returns a list of [RPChoice]s",
     () {
       final List<RPChoice> choicesExpected = <RPChoice>[
