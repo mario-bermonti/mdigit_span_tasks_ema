abf64f47ae9a51b95c4326c3477ca052aa205d18
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-06-25 11:30:51 -0400

    improve: Integrate question builders to controller
    improve: Integrate question builders to controller
    
    improve: Integrate question builders to controller
    
    refactor: Move sexQuestion to controller
    
    improve: Init sexQuestion in controller
    
    refactor: Rename baseline_survey_controller
    
    improve: Define survey controller fields
    
    improve: Impl fun to load questions
    
    refactor: Load baseline questions during init
    
    fix: Apply several fixes to baseline controller
    
    fix: Add base path to load json questions
    
    fix: Set isLoading to false when finished loading
    
    test: Fix tests baseline survey controller
    
    improve: Build baseline questions from a list
    
    The filenames containing the information for
    building the baseline questions are now stored in
    a list to facilitate building them and keeping
    code maintainable.
    
    refactor: Make controller params consistent
    
    doc: Add docstring to loading questions

--- test/src/baseline/baseline_survey_controller_test.dart
+++ test/src/baseline/baseline_survey_controller_test.dart
@@ -1,11 +1,18 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mdigit_span_tasks_ema/src/baseline/question_builders.dart';
+import 'package:get/get.dart';
+import 'package:mdigit_span_tasks_ema/src/baseline/baseline_survey_controller.dart';
 import 'package:research_package/model.dart';
 
 void main() {
+  late BaselineSurveyController controller;
+
+  setUp(() {
+    controller = Get.put(BaselineSurveyController());
+  });
+
   test(
     "Given a map with identifier, title, and choices, returns an RPQuestionStep",
-    () {
+    () async {
       final List<RPChoice> choicesExpected = <RPChoice>[
         RPChoice(text: 'black', value: 0),
         RPChoice(text: 'white', value: 1),
@@ -38,7 +45,7 @@ void main() {
       };
 
       final RPQuestionStep questionActual =
-          buildSingleChoiceQuestion(config: questionJson);
+          controller.buildSingleChoiceQuestion(config: questionJson);
 
       expect(questionActual.identifier, questionExpected.identifier);
       expect(questionActual.title, questionExpected.title);
@@ -73,7 +80,8 @@ void main() {
         },
       ];
 
-      final List<RPChoice> choicesActual = buildChoices(choices: choicesRaw);
+      final List<RPChoice> choicesActual =
+          controller.buildChoices(choices: choicesRaw);
 
       expect(choicesActual.first.toJson(), choicesExpected.first.toJson());
       expect(choicesActual.last.toJson(), choicesExpected.last.toJson());
