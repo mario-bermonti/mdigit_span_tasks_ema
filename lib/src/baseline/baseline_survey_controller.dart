a929671370f2c842647c4736741282d1d2cba0c9
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-07-02 15:32:27 -0400

    feat: Add sex question to baseline survey

--- lib/src/baseline/baseline_survey_controller.dart
+++ lib/src/baseline/baseline_survey_controller.dart
@@ -6,7 +6,7 @@ import 'package:research_package/research_package.dart';
 
 class BaselineSurveyController extends GetxController {
   final String basePath = 'assets/baseline_questions';
-  final List<String> questionFilenames = <String>[];
+  final List<String> questionFilenames = <String>['sex.json'];
   final List<RPQuestionStep> questions = <RPQuestionStep>[].obs;
   RxBool isLoading = true.obs;
   RxBool hasError = false.obs;
