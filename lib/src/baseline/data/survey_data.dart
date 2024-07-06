564a1c96c487a29df7d5b7ccd968072a5e827534
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-07-06 18:09:56 -0400

    docs: Add docstring to survey data model

--- lib/src/baseline/data/survey_data.dart
+++ lib/src/baseline/data/survey_data.dart
@@ -1,10 +1,18 @@
 import 'survey_item_data.dart';
 
 class SurveyData {
+  /// Short description of the item
   final String identifier;
+
+  /// Longer description than the [identifier].
+  /// It's usually the text presented to the participant or
+  /// a long description.
   final String description;
   final String startTime;
   final String endTime;
+
+  /// List of survey items that include the data about the item and
+  /// the participant's responses.
   final List<SurveyItemData> items;
 
   SurveyData({
