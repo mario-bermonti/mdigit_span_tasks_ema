70af34c7673e4191f93fb567298af6c845d6a3cc
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-07-06 16:23:18 -0400

    feat: Define a survey item model
    feat: Define a survey item model
    
    improve: Convert SurveyItemData into freezed obj
    
    improve: Convert SurveyItemData into freezed obj
    
    SurveyItemData - remove g.dart
    
    improve: SurveyItemData - Add auto freezed file
    
    buildSurveyItemData - make choices optional
    
    buildSurveyItemData - make choices optional
    
    SurveyItemData - make choices optional, add file
    
    fix: Make start, end times optional survey items

---
+++ lib/src/baseline/data/survey_item_data.dart
@@ -0,0 +1,29 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:flutter/foundation.dart';
+
+part 'survey_item_data.freezed.dart';
+
+/// Model that represents the data for a single survey item.
+@freezed
+class SurveyItemData with _$SurveyItemData {
+  const factory SurveyItemData({
+    required DateTime? startTime,
+    required DateTime? endTime,
+
+    /// Short description of the item
+    required String identifier,
+
+    /// Longer description than the [identifier].
+    /// It's usually the text presented to the participant or
+    /// a long description.
+    required String description,
+
+    /// Type of survey item present to understand the response scale.
+    /// Common options are single choice, multiple choice, free text.
+    required String type,
+    required String response,
+
+    /// Possible choices the participant select from.
+    List<String>? choices,
+  }) = _SurveyItemData;
+}
