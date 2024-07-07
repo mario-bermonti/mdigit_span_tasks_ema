cb62c271d581e96f880a1ae7297ed19fc83ab916
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-07-06 20:19:57 -0400

    feat: Add base for buildSurveyItemData()

--- lib/src/baseline/services/rp_survey_data_converter.dart
+++ lib/src/baseline/services/rp_survey_data_converter.dart
@@ -2,6 +2,8 @@ import 'dart:convert';
 
 import 'package:research_package/model.dart';
 
+import '../data/survey_item_data.dart';
+
 /// Convert a [research_package]'s [RPTaskResult] to a json object.
 Map<String, dynamic> rpSurveyDataToJson(RPTaskResult results) {
   /// convert to string first to get the nested objects.
@@ -9,3 +11,7 @@ Map<String, dynamic> rpSurveyDataToJson(RPTaskResult results) {
   final Map<String, dynamic> jsonResults = jsonDecode(stringResults);
   return jsonResults;
 }
+
+List<SurveyItemData> buildSurveyItemData({
+  required Map<String, dynamic> jsonItem,
+}) {}
