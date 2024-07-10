537334818048825e03a90b67b1eb5cc2582cf0e0
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-07-10 09:38:43 -0400

    Delete - design of interface impl

---
+++ lib/src/baseline/services/new_concep.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:research_package/model.dart';
+
+/// models
+class SurveyData {
+  fromJson() {}
+  toJson() {}
+}
+
+class SurveyItemData {
+  fromJson() {}
+  toJson() {}
+}
+
+/// service
+
+/// high level function used by package
+SurveyData buildSurveyData(RPTaskResult rpTaskResult) {
+  /// format survey metadata
+  /// format survey items data - fun
+  /// Build SurveyData object
+}
+
+// Converter object
+/// helper functions
+
+Map<String, dynamic> formatSurveyMetadata(RPTaskResult rpTaskResult) {
+  /// extract data and convert it into map
+}
+Map<String, dynamic> formatSurveyItemsData(Map<String, RPResult> items) {
+  /// define formatted map to store formatted items
+  /// results map: key = id, value = data
+  /// iterates over results and passes value to item formatting fun
+}
+
+Map<String, dynamic> formatSurveyItemData(RPResult item) {
+  /// extract metadata into formatted json object
+  /// format choices
+  /// format answer
+}
+
+Map<String, dynamic> rpResultsToJson(RPResult item) {
+  /// extract data and make it available
+  // debugging
+  // print('single item');
+  // print(jsonDecode(jsonEncode(results.results.values.first.toJson())));
+}
+
+Map<String, dynamic> extractItemMetadata(Map<String, dynamic> item) {
+  /// extract item metadata
+}
+
+/// formatSurveyItemData helper
+dynamic formatAnswer(dynamic answer, ItemType itemType) {
+  /// if string, return it
+  /// if list, format it
+}
+
+/// formatAnswer helper
+String formatSingleChoiceAnswer(List<Map<String, dynamic>> answer) {
+  /// extract label from answer
+}
+
+List<String>? formatChoices(dynamic choices) {
+  /// if null, return null?
+  /// if list, extract labels from subitems
+}
+
+List<String> extractLabelsSubitems(List<Map<String, dynamic>> subitems) {
+  /// iterate over subitems and extract label
+}
