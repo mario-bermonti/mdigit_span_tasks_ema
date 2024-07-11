b10b543c71f164686120e3f5e47f1b077cb36fce
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-07-11 14:44:23 -0400

    feat: Impl RPResult to json converter
    singleRPResultToJson - failing base
    
    singleRPResultToJson rename to RPResultToJson
    
    singleRPResultToJson - comment guide
    
    rpResultToJson - failing impl
    
    rpResultToJson - fix param type
    
    rpResultToJson - maint
    
    rpResultToJson rename and fix doc

--- lib/src/baseline/rp_to_survey_data_converter.dart
+++ lib/src/baseline/rp_to_survey_data_converter.dart
@@ -1,4 +1,17 @@
+import 'dart:convert';
+
+import 'package:research_package/research_package.dart';
+
 /// Converts an [RPTaskResult] from the [research_package] object into a
 /// [SurveyData] object by extracting the relevant data and formatting it
 /// appropriately.
-class RPToSurveyDataConverter {}
+class RPToSurveyDataConverter {
+  /// Converts an [RPStepResult] from the [research_package] to a json object.
+  /// This makes available all the data inside the [RPStepResult] and makes
+  /// it easier to handle.
+  Map<String, dynamic> rpStepResultToJson(RPStepResult item) {
+    final String stringItem = jsonEncode(item.toJson());
+    final Map<String, dynamic> jsonItem = jsonDecode(stringItem);
+    return jsonItem;
+  }
+}
