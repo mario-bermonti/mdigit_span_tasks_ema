f39699191efabf7d09411ba1fb925b7ff8eeee24
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-07-12 14:31:58 -0400

    feat: Define base for RPToSurveyDataConverter

---
+++ lib/src/baseline/rp_to_survey_data_converter.dart
@@ -0,0 +1,4 @@
+/// Converts an [RPTaskResult] from the [research_package] object into a
+/// [SurveyData] object by extracting the relevant data and formatting it
+/// appropriately.
+class RPToSurveyDataConverter {}
