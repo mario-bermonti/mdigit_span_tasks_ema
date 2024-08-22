1200ebd3191839bc64cf3738eba37502cb67b6db
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-08-22 08:37:14 -0400

    feat: Return baseline survey data when survey ends
    feat: Return baseline survey data when survey ends
    
    endBaselineSurvey - impl
    
    endBaselineSurvey - impl update

--- lib/src/baseline/baseline_survey_screen.dart
+++ lib/src/baseline/baseline_survey_screen.dart
@@ -1,8 +1,7 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:mdigit_span_tasks_ema/src/baseline/data/survey_data.dart';
 import 'package:mdigit_span_tasks_ema/src/ui_components/loading_screen.dart';
 import 'package:research_package/model.dart';
 import 'package:research_package/ui.dart';
@@ -28,14 +27,22 @@ class BaselineSurveyScreen extends StatelessWidget {
 
         return RPUITask(
           task: survey,
-          onSubmit: (RPTaskResult results) async {
-            print('baseline data');
-            print(jsonEncode(results.toJson()));
-            GetStorage().write('baselineCompleted', true);
-            await Get.toNamed('/tasklist');
-          },
+          onSubmit: (RPTaskResult results) => endBaselineSurvey(results),
         );
       }
     });
+  }
+
+  /// Prepares the survey data for exporting, marks the baseline survey as
+  /// completed, and moves to the next screen.
+  Future<SurveyData> endBaselineSurvey(RPTaskResult results) async {
+    final SurveyData surveyData = SurveyData.fromRPTaskResult(
+      rpSurveyData: results,
+      description: 'Baseline demographics survey',
+    );
+    print('survey data ${surveyData.toJson()}');
+    GetStorage().write('baselineCompleted', true);
+    await Get.toNamed('/tasklist');
+    return surveyData;
   }
 }
