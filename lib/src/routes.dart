ac9daf9a3d3285a7d666a49f7194aa89542e5d9c
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-08-26 09:18:23 -0400

    main: Remove deprecated initial route manager
    Remove initial route manangers
    
    Superseeded by the middleware implementation
    
    main: Remove import deprecated initial route manager

--- lib/src/routes.dart
+++ lib/src/routes.dart
@@ -1,6 +1,5 @@
 import 'package:get/get.dart';
 import 'package:mdigit_span_tasks_ema/src/ema/ema_screen.dart';
-import 'package:mdigit_span_tasks_ema/src/initial_route_manager/initial_route_manager_view.dart';
 import 'package:mdigit_span_tasks_ema/src/task_list/view/task_list_page.dart';
 import 'package:mdigit_span_tasks_ema/src/ui_components/loading_screen.dart';
 
