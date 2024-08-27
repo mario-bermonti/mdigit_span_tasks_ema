87920b23218f17056c7643404923b5806b818e11
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-08-27 08:47:19 -0400

    refactor: Rename file that ends demo survey

--- lib/src/baseline/baseline_survey_screen.dart
+++ lib/src/baseline/baseline_survey_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:mdigit_span_tasks_ema/src/demographics/process_demographics_data.dart';
+import 'package:mdigit_span_tasks_ema/src/demographics/end_demographics_survey.dart';
 import 'package:mdigit_span_tasks_ema/src/ui_components/loading_screen.dart';
 import 'package:research_package/model.dart';
 import 'package:research_package/ui.dart';
--- lib/src/routes.dart
+++ lib/src/routes.dart
@@ -1,5 +1,5 @@
 import 'package:get/get.dart';
-import 'package:mdigit_span_tasks_ema/src/demographics/process_demographics_data.dart';
+import 'package:mdigit_span_tasks_ema/src/demographics/end_demographics_survey.dart';
 import 'package:mdigit_span_tasks_ema/src/ema/ema_screen.dart';
 import 'package:mdigit_span_tasks_ema/src/task_list/view/task_list_page.dart';
 import 'package:mdigit_span_tasks_ema/src/ui_components/loading_screen.dart';
