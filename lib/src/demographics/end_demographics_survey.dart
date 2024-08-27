14dde587fa0a46aab6b825409f2fb64e1210f1f9
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-08-27 08:54:41 -0400

    refactor: Extract processing data into fun
    refactor: Extract processing data into fun
    
    doc: Add docstring to fun that processes demo data

--- lib/src/demographics/end_demographics_survey.dart
+++ lib/src/demographics/end_demographics_survey.dart
@@ -1,18 +1,12 @@
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:mdigit_span_tasks_ema/src/demographics/process_demographics_data.dart';
 import 'package:research_package/research_package.dart';
 
-import '../baseline/data/survey_data.dart';
-
-/// Prepares the survey data for exporting, marks the baseline survey as
+/// Processes the survey data for exporting, marks the baseline survey as
 /// completed, and moves to the next screen.
-Future<SurveyData> endDemographicsSurvey(RPTaskResult results) async {
-  final SurveyData surveyData = SurveyData.fromRPTaskResult(
-    rpSurveyData: results,
-    description: 'Baseline demographics survey',
-  );
-  print('external survey data ${surveyData.toJson()}');
+void endDemographicsSurvey(RPTaskResult results) {
+  processDemographicsData(results);
   GetStorage().write('baselineCompleted', true);
-  await Get.toNamed('/tasklist');
-  return surveyData;
+  Get.toNamed('/tasklist');
 }
