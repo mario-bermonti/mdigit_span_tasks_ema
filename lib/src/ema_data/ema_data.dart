76cf8791b411a0a165e3c0335de592b8518c16da
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-08-29 09:12:53 -0400

    feat: Add survey data to FB
    feat: Add survey data to FB
    
    feat: Add survey data to FB
    
    test: Add setup for testing ema data manager
    
    test: Test adding cross-sectional survey to FB
    
    test: Test adding cross-sectional items survey to FB
    
    improve: Def base adding cross-sectional survey
    
    feat: Add survey metadata to firebase
    
    feat: Add survey items to Fb
    
    test: Separate tests for adding data to fb
    
    refactor: Extract method to add survey metadata
    
    refactor: Extract method to add survey item data
    
    refactor: Reorg and simplify code
    
    refactor: Clean and add docstrings
    
    refactor: Rename variables in ema data manager
    
    test: Test subfuns for adding survey data
    
    fix tests and improve fun params

--- lib/src/ema_data/ema_data.dart
+++ lib/src/ema_data/ema_data.dart
@@ -1,4 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:mdigit_span_tasks_ema/src/baseline/data/survey_data.dart';
+import 'package:mdigit_span_tasks_ema/src/baseline/data/survey_item_data.dart';
 
 class EMAData {
   final FirebaseFirestore db;
@@ -8,4 +10,59 @@ class EMAData {
     this.db, {
     required this.participantID,
   });
+
+  /// Adds data from a cross-sectional survey data to Firebase
+  Future<void> addCrossSectionalSurveyData({
+    required String measureName,
+    required SurveyData data,
+  }) async {
+    final Map<String, dynamic> surveyData = data.toJson();
+
+    await addCrossSectionalSurveyItemData(
+      measureName: measureName,
+      data: surveyData['items'],
+    );
+
+    surveyData.remove('items');
+    await addCrossSectionalSurveyMetadata(
+      measureName: measureName,
+      data: surveyData,
+    );
+  }
+
+  /// Add data from cross-sectional items to Firebase
+  Future<void> addCrossSectionalSurveyItemData({
+    required String measureName,
+    required List<SurveyItemData> data,
+  }) async {
+    final CollectionReference itemsRef = db
+        .collection('cross_sectional')
+        .doc(measureName)
+        .collection('participants')
+        .doc(participantID)
+        .collection('items');
+
+    final WriteBatch batch = db.batch();
+
+    for (SurveyItemData item in data) {
+      Map<String, dynamic> jsonItem = item.toJson();
+      batch.set(itemsRef.doc(item.identifier), jsonItem);
+    }
+    await batch.commit();
+  }
+
+  /// Add metadata from a cross-sectional survey to Firebase
+  Future<void> addCrossSectionalSurveyMetadata({
+    required String measureName,
+    required Map<String, dynamic> data,
+  }) async {
+    final CollectionReference metadataRef = db
+        .collection('cross_sectional')
+        .doc(measureName)
+        .collection('participants')
+        .doc(participantID)
+        .collection('metadata');
+
+    await metadataRef.doc('metadata').set(data);
+  }
 }
