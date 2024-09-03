99119631020fc485c1e0197befadc9860ea23f8f
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-09-03 10:03:07 -0400

    improve: Make Participant controller available

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:mdigit_span_tasks_ema/src/app.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:mdigit_span_tasks_ema/src/auth/auth.dart';
+import 'package:mdigit_span_tasks_ema/src/auth/participant.dart';
 import 'package:mdigit_span_tasks_ema/src/digit_span_tasks/config/config.dart';
 import 'package:mdigit_span_tasks_ema/src/notifications/local_notifications.dart';
 import 'package:mdigit_span_tasks_ema/src/notifications/firebase_notifications.dart';
@@ -16,6 +19,9 @@ Future<void> main() async {
   await GetStorage.init();
   Get.put(LocalNotifications());
   Get.put(FirebaseNotifications());
+  final Participant participant =
+      await Auth(auth: FirebaseAuth.instance).signIn();
+  Get.put(participant);
   Get.put(DigitSpanTaskConfig(), permanent: true);
   runApp(const MyApp());
 }
