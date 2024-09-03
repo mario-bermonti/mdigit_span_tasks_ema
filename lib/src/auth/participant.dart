d51adc26b3e574b7de7f4d738d8d3205412cafef
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-09-03 09:46:53 -0400

    improve: Make Participant a controller

--- lib/src/auth/participant.dart
+++ lib/src/auth/participant.dart
@@ -1,7 +1,9 @@
+import 'package:get/get.dart';
+
 /// Represents a single study participant and performs authentication
 /// Can be used to keep track of participants' important info, including
 /// "identifying" info.
-class Participant {
+class Participant extends GetxController {
   final String id;
 
   Participant({
