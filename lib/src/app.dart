7f9e95290daabcdabe43a399aa25e47ba784d35c
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-09-09 09:28:53 -0400

    feat: Config app internationalization
    feat: Config app internationalization
    
    feat: Config app internationalization
    
    feat: Config app internationalization
    
    feat: Config app internationalization
    
    refactor: Remove app internalization from app code
    
    doc: Add docstrings to internationalization
    
    refactor: Add types to function that resolves intl

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:get/get.dart';
 import 'package:mdigit_span_tasks_ema/src/routes.dart';
+import 'package:research_package/research_package.dart';
+
+import 'intl/internationalization.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -13,6 +17,10 @@ class MyApp extends StatelessWidget {
       initialRoute: '/',
       theme: ThemeData(primarySwatch: Colors.grey),
       getPages: routes,
+      locale: locale,
+      supportedLocales: supportedLocales,
+      localizationsDelegates: localizationsDelegates,
+      localeResolutionCallback: resolveLocale,
     );
   }
 }
