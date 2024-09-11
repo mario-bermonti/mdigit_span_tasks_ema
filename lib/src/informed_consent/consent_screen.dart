aff92e72b17b11d872492d0ebefeac8c35796a55
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-09-11 17:09:19 -0400

    improve: Insert consent controller

--- lib/src/informed_consent/consent_screen.dart
+++ lib/src/informed_consent/consent_screen.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/widgets.dart';
+import 'package:get/get.dart';
+import 'package:mdigit_span_tasks_ema/src/informed_consent/consent_controller.dart';
 
 class ConsentScreen extends StatelessWidget {
-  const ConsentScreen({super.key});
+  final ConsentController _controller = Get.find();
+
+  ConsentScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
