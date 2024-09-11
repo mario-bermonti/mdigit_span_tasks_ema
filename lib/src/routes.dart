1227b10d7c932241b14a79ac51b9cba6e028e763
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-09-11 17:03:24 -0400

    feat: Add consent screen route
    feat: Add consent screen route
    
    fix: Make consent route non-constant

--- lib/src/routes.dart
+++ lib/src/routes.dart
@@ -1,6 +1,7 @@
 import 'package:get/get.dart';
 import 'package:mdigit_span_tasks_ema/src/demographics/end_demographics_survey.dart';
 import 'package:mdigit_span_tasks_ema/src/ema/ema_screen.dart';
+import 'package:mdigit_span_tasks_ema/src/informed_consent/consent_screen.dart';
 import 'package:mdigit_span_tasks_ema/src/task_list/view/task_list_page.dart';
 import 'package:mdigit_span_tasks_ema/src/ui_components/loading_screen.dart';
 
@@ -14,6 +15,7 @@ final List<GetPage> routes = <GetPage>[
       page: () => const HomeScreen(),
       middlewares: [InitialRouteMiddleware()]),
   GetPage(name: '/tasklist', page: () => const TaskListPage()),
+  GetPage(name: '/consent', page: () => ConsentScreen()),
   GetPage(
       name: '/demographicsSurvey',
       page: () => DemographicsSurvey(
