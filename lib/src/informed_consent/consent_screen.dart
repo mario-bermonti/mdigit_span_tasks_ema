4a3af1ae18da1792624fd5c0b4c1d224da1cb74a
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-09-11 17:04:53 -0400

    feat: Add basic consent screen

---
+++ lib/src/informed_consent/consent_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/widgets.dart';
+
+class ConsentScreen extends StatelessWidget {
+  const ConsentScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Consent'),
+    );
+  }
+}
