1466a74f6f32f1d00b3e4c8ef5a667b035895f3c
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-09-17 08:02:27 -0400

    feat: Add content to summary and full consent

--- lib/src/informed_consent/consent_objects.dart
+++ lib/src/informed_consent/consent_objects.dart
@@ -1,51 +1,83 @@
 import 'package:research_package/research_package.dart';
 
 RPConsentSection overviewSection = RPConsentSection(
-    type: RPConsentSectionType.Overview,
-    summary: "overview.title",
-    content: "overview.content");
+  type: RPConsentSectionType.Overview,
+  summary:
+      "Usamos mDigits para estudiar cómo las personas recuerdan información reciente",
+  content:
+      """mDigits nos permite estudiar cómo funciona la memoria de las personas para recordar información reciente en la vida cotidiana. Por ejemplo, cuán bien recordamos información luego de caminar o ver una película.
+
+Usamos esta información para entender cómo funciona la memoria de las personas y cómo podemos mejorarla.""",
+);
 
 RPConsentSection aboutUs = RPConsentSection(
-    type: RPConsentSectionType.AboutUs,
-    summary: "This is a summary for About Us.",
-    content: "Info about us");
+  type: RPConsentSectionType.AboutUs,
+  summary: "¡Somos el CHTLab!",
+  content:
+      """Somos el laboratorio de investigación Cognition, Health, and Technology (CHTLab) de la Ponce Health Sciences University (PHSU).
+      
+En el CHTLab estudiamos cómo la memoria, la atención y otros procesos cognitivos que nos permiten pensar se relacionan con la salud y cómo podemos mejorarlos.""",
+);
 
 RPConsentSection dataGatheringSection = RPConsentSection(
     type: RPConsentSectionType.DataGathering,
-    summary: "datagathering.title",
-    content: "datagathering.content");
-
-RPConsentSection privacySection = RPConsentSection(
-    type: RPConsentSectionType.Privacy,
-    summary: "privacy.title",
-    content: "privacy.content");
-
-RPConsentSection timeCommitmentSection = RPConsentSection(
-    type: RPConsentSectionType.TimeCommitment,
-    summary: "This is a summary for Time Commitment.",
+    summary:
+        "Recogemos información sobre cuán bien las personas recuerdan información nueva",
     content:
-        "Time commitment dolor sit amet, consectetur adipiscing elit. Aenean a mi porttitor, bibendum elit elementum, placerat augue. Quisque eu sollicitudin tortor, sed egestas ante. Sed convallis, mauris quis malesuada convallis, lectus ante vestibulum ante, vel lobortis magna dui eu nisl. Proin ac pellentesque nulla. Morbi facilisis dui aliquam quam pulvinar efficitur. Duis at lorem vitae leo pharetra ultricies. Proin viverra eleifend varius. Nulla sed nisi ut enim placerat venenatis. Maecenas imperdiet accumsan ligula id varius. Donec rhoncus gravida odio vitae convallis.Nullam at tempor erat. Praesent euismod orci nec sollicitudin placerat. Nunc nec nibh efficitur, mattis ante sit amet, scelerisque libero. Aliquam et mollis erat. Pellentesque aliquam convallis turpis sit amet molestie. Duis accumsan venenatis imperdiet. Integer quis est non elit varius mattis. Donec hendrerit in nisl eget sollicitudin. Nulla sapien lacus, mattis non orci sed, commodo tincidunt risus.");
+        """mDigits nos permite recoger información sobre cuán bien recuerdas información nueva completando actividades de memoria.
+
+También recogemos alguna información básica sobre ti como tu edad y nivel educativo. 
+        
+Completarás todas las actividades en este celular.""");
 
 RPConsentSection studyTasksSection = RPConsentSection(
-    title: "What should you do?",
     type: RPConsentSectionType.StudyTasks,
-    summary: "This is a summary for Study Tasks.",
-    content: "Study Tasks dolor sit amet");
+    summary: "Completarás unas actividades de memoria",
+    content: """En estas actividades verás una lista de números. 
+
+En algunas ocasiones recordarás la lista de números en el orden en que los viste. 
+
+En otras ocasiones recordarás la lista de atrás para adelante.
+
+mDigits te guiará para completar las actividades con instrucciones claras y sencillas. También completarás ejercicios de practica.""");
 
 RPConsentSection studySurveySection = RPConsentSection(
     type: RPConsentSectionType.StudySurvey,
-    summary: "This is a summary for Study Survey.",
-    content: "Study Survey dolor sit amet");
+    summary: "Completarás cuestionarios sencillos",
+    content:
+        """Completarás un cuestionario con información básica sobre ti como tu 
+        edad y nivel educativo.
+        
+También completarás un cuestionario sobre tu salud como si padeces condiciones de salud como diabetes o alta presión.""");
+
+RPConsentSection privacySection = RPConsentSection(
+    type: RPConsentSectionType.Privacy,
+    summary: "No parearemos tus datos con tu nombre y  protegeremos tus datos",
+    content:
+        """No preguntaremos tu nombre, número de teléfono ni ninguna otra información que pueda identificarte. Así que nadie podrá saber que los datos te pertenecen a ti.
+        
+También protegeremos tus datos convirtiéndolos en un código secreto usando un proceso llamado "encriptar".""");
+
+RPConsentSection timeCommitmentSection = RPConsentSection(
+    type: RPConsentSectionType.TimeCommitment,
+    summary: "Completarás las actividades de memoria diariamente por 2 semanas",
+    content:
+        """Completarás las actividades de memoria 3 veces al día durante la mañana, tarde y noche.
+        
+Completarás las actividades durante 2 semanas.""");
 
 RPConsentSection welcomeSection = RPConsentSection(
     type: RPConsentSectionType.Welcome,
-    summary: "This is a summary for Welcome.",
-    content: "Welcome dolor sit amet");
+    summary: "¡Te damos la bienvenida al equipo mDigits!",
+    content:
+        """Tu participación es sumamente importante y nos ayudará a entender 
+cómo funciona la memoria cuando tratamos de recordar información nueva.""");
 
 RPConsentSection withdrawingSection = RPConsentSection(
     type: RPConsentSectionType.Withdrawing,
-    summary: "This is a summary for Withdrawing.",
-    content: "Withdrawing dolor sit amet");
+    summary: "Puedes terminar de participar en cualquier momento",
+    content:
+        "Solo tienes que desinstalar la aplicación si decides no continuar participando en el estudio.");
 
 RPConsentDocument consentDocumentSummary = RPConsentDocument(
   title: 'Informed Consent - Summary',
@@ -75,7 +107,7 @@ RPConsentDocument consentDocumentFull = RPConsentDocument(
 );
 
 RPConsentReviewStep consentReviewStep = RPConsentReviewStep(
-  identifier: "consentreviewstepID",
+  identifier: "consentReview",
   title: 'Consent Review',
   consentDocument: consentDocumentFull,
   reasonForConsent: 'informed_consent.agree_text',
@@ -86,16 +118,21 @@ RPVisualConsentStep consentVisualStep = RPVisualConsentStep(
     identifier: "visualStep", consentDocument: consentDocumentSummary);
 
 RPInstructionStep instructionStep = RPInstructionStep(
-  identifier: "instructionID",
-  title: "Welcome!",
+  identifier: "consentInstruction",
+  title: "¡Te damos la bienvenida!",
   detailText:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ultricies feugiat turpis nec efficitur. Integer in pharetra libero. Proin a leo eu enim porttitor hendrerit. Suspendisse vestibulum interdum mollis. Donec in sapien ut orci ultricies laoreet. Ut maximus ante id arcu feugiat scelerisque. Proin non rutrum libero. Aliquam blandit arcu ac dolor consequat maximus. Integer et dolor quis quam tempor porta quis vel nibh. Phasellus ullamcorper fringilla lorem, ac tempus sem cursus a. Aliquam maximus facilisis quam. Morbi hendrerit tempor tellus, ac hendrerit augue tincidunt eu. Cras convallis lorem at nulla mattis tristique.",
-  footnote: "(1) Important footnote",
-  text: "informed_consent.welcome_text",
+      """mDigits es parte de un estudio de investigación realizado por la Ponce Health Sciences University (PHSU).
+
+A continuación te ofreceremos información sobre el estudio para que puedes decidir si te interesa participar.
+      
+Te adelantamos que tu participación será sencilla y nos ayudará mucho.
+
+¡Gracias por tu apoyo!""",
+  text: "¡Nos alegra tu interés en nuestro estudio!",
 );
 
 RPCompletionStep completionStep = RPCompletionStep(
-  identifier: "completionID",
-  title: "informed_consent.thanks",
-  text: "informed_consent.document_saved",
+  identifier: "consentCompletion",
+  title: "¡Gracias por tu apoyo!",
+  text: "Guardamos tu respuesta",
 );
