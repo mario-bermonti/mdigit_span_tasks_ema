6af03cb84da01477e52d6c5d6c30b27b88cae727
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-09-24 09:14:21 -0400

    doc: Add docstring to consent documents

--- lib/src/informed_consent/consent_docs.dart
+++ lib/src/informed_consent/consent_docs.dart
@@ -2,6 +2,8 @@ import 'package:research_package/research_package.dart';
 
 import 'consent_sections.dart';
 
+/// Summary of the informed consent form that is presented interactively and
+/// engagingly before the formal full consent form.
 RPConsentDocument consentDocumentSummary = RPConsentDocument(
   title: 'Informed Consent - Summary',
   sections: [
@@ -15,6 +17,9 @@ RPConsentDocument consentDocumentSummary = RPConsentDocument(
   ],
 );
 
+/// Full formal version of the informed consent form.
+/// It is presented during the consent review after the summary consent form. It
+/// was designed to present the formal consent that is usually required by IRBs.
 RPConsentDocument consentDocumentFull = RPConsentDocument(
   title: 'Informed Consent - Full',
   sections: [
