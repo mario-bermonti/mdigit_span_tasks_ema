25a52b417310284b79cbf96184bf2c4d1484288d
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-11-19 09:01:17 -0400

    fix: Remove obx from landing screen

--- lib/src/landing/landing_screen.dart
+++ lib/src/landing/landing_screen.dart
@@ -3,7 +3,6 @@ import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:mdigit_span_tasks_ema/src/ema/ema_screen.dart';
 import 'package:mdigit_span_tasks_ema/src/task_list/view/task_list_page.dart';
-import 'package:mdigit_span_tasks_ema/src/ui_components/loading_screen.dart';
 
 import '../demographics/end_demographics_survey.dart';
 import '../informed_consent/consent_screen.dart';
@@ -16,22 +15,17 @@ class LandingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Obx(
-      () {
-        if (_controller.nextScreen.isEmpty) {
-          return const LoadingScreen();
-        } else if (_controller.nextScreen.value == 'consent') {
-          return ConsentScreen();
-        } else if (_controller.nextScreen.value == 'demographicsSurvey') {
-          return DemographicsSurvey(
-            onSubmit: endDemographicsSurvey,
-          );
-        } else if (_controller.nextScreen.value == 'emaScreen') {
-          return const EMAScreen();
-        } else {
-          return const TaskListPage();
-        }
-      },
-    );
+    _controller.determineNextScreen();
+    if (_controller.nextScreen.value == 'consent') {
+      return ConsentScreen();
+    } else if (_controller.nextScreen.value == 'demographicsSurvey') {
+      return DemographicsSurvey(
+        onSubmit: endDemographicsSurvey,
+      );
+    } else if (_controller.nextScreen.value == 'emaScreen') {
+      return const EMAScreen();
+    } else {
+      return const TaskListPage();
+    }
   }
 }
