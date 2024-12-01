9c1d6d542a998bbbc48dcc093b72b89e1647771c
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-11-30 22:02:51 -0400

    improve: Define basic participant model

---
+++ participant/participant_model.dart
@@ -0,0 +1,10 @@
+/// Represents a single study participant and performs authentication
+/// Can be used to keep track of participants' important info, including
+/// "identifying" info.
+class ParticipantModel {
+  final String id;
+
+  ParticipantModel({
+    required this.id,
+  });
+}
