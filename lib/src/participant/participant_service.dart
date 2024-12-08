ce1ae4ce80770ebcea1aa352341e2a9068624e77
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-12-08 14:14:06 -0400

    improve: Impl a basic participant service

---
+++ lib/src/participant/participant_service.dart
@@ -0,0 +1,14 @@
+import 'package:mdigit_span_tasks_ema/src/participant/participant_model.dart';
+
+/// Exposes the participant data app-wide.
+///
+/// [uid] is the unique identifier of the participant.
+class ParticipantService {
+  ParticipantModel? _participant;
+
+  ParticipantModel? get participant => _participant;
+
+  ParticipantService({required String uid}) {
+    _participant = ParticipantModel(uid: uid);
+  }
+}
