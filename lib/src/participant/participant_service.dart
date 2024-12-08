791085764a1be82c033fd46ce1c091d098a77315
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-12-08 15:16:53 -0400

    feat: Impl fun to add participant fcm tokens

--- lib/src/participant/participant_service.dart
+++ lib/src/participant/participant_service.dart
@@ -12,4 +12,10 @@ class ParticipantService {
   ParticipantService({required String uid}) {
     _participant = ParticipantModel(uid: uid);
   }
+
+  /// Updates the participant's FCM tokens.
+  void addFcmTokens({required String fcmToken}) {
+    final List<String> fcmTokens = [..._participant.fcmTokens, fcmToken];
+    _participant = _participant.copyWith(fcmTokens: fcmTokens);
+  }
 }
