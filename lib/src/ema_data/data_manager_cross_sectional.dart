aca6e24cb0a17d1639494fe0081a6c668cb06615
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-12-12 13:34:10 -0400

    improve: Define manager for cross-sectional data

---
+++ lib/src/ema_data/data_manager_cross_sectional.dart
@@ -0,0 +1,18 @@
+import 'data_manager.dart';
+
+/// Manages cross-sectional data in the dbs.
+class DataManagerCrossSectional implements DataManager {
+  /// Add metadata from cross-sectional items to db.
+  @override
+  Future<void> saveMetadata() {
+    // TODO: implement saveMetadata
+    throw UnimplementedError();
+  }
+
+  /// Add data from cross-sectional items to db.
+  @override
+  Future<void> saveItemData() {
+    // TODO: implement saveItemData
+    throw UnimplementedError();
+  }
+}
