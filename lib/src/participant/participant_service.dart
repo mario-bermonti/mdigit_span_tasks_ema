054af51653dd06b311ea7ecc1f1ca479e264afbb
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-12-12 10:47:13 -0400

    improve: Require participant repo in its service

--- lib/src/participant/participant_service.dart
+++ lib/src/participant/participant_service.dart
@@ -1,15 +1,22 @@
 import 'package:mdigit_span_tasks_ema/src/participant/participant_model.dart';
+import 'package:mdigit_span_tasks_ema/src/participant/participant_repository.dart';
 
 /// Exposes the participant data app-wide.
 ///
 /// [uid] is the unique identifier of the participant.
+/// [participantRepository] contains the logic for saving participant data to
+/// dbs.
 class ParticipantService {
   late ParticipantModel _participant;
+  final ParticipantRepository participantRepository;
 
   /// Data about the current participant.
   ParticipantModel get participant => _participant;
 
-  ParticipantService({required String uid}) {
+  ParticipantService({
+    required String uid,
+    required this.participantRepository,
+  }) {
     _participant = ParticipantModel(uid: uid);
   }
 
