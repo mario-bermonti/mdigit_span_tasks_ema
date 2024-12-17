7d7981fc0e0cc3904190cf21b2c9e6a3458c8879
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-12-17 12:11:02 -0400

    improve: Require Metadata in firebase data source

--- lib/src/ema_db/ema_firebase_data_source.dart
+++ lib/src/ema_db/ema_firebase_data_source.dart
@@ -1,10 +1,11 @@
 import 'data_source.dart';
+import 'models/metadata/metadata.dart';
 
 /// Data source that interacts with Firebase db.
 class FirebaseDataSource implements DataSource {
   /// Add metadata about the task.
   @override
-  Future<void> saveTaskMetadata() {
+  Future<void> saveTaskMetadata({required Metadata metadata}) {
     // TODO: implement saveMetadata
     throw UnimplementedError();
   }
