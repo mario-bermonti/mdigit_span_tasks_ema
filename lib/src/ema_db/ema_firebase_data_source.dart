983c1e605187587ae89991a0dbc29f779a65db66
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-12-19 09:54:08 -0400

    refactor: Remove data source ref from fb impl

--- lib/src/ema_db/ema_firebase_data_source.dart
+++ lib/src/ema_db/ema_firebase_data_source.dart
@@ -1,23 +1,23 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-import 'data_source.dart';
 import 'models/metadata/metadata.dart';
 
 /// Data source that interacts with Firebase db.
-class FirebaseDataSource implements DataSource {
+class FirebaseDataSource {
   final FirebaseFirestore db;
 
   FirebaseDataSource({required this.db});
 
   /// Add metadata about the task.
-  @override
-  Future<void> saveTaskMetadata({required Metadata metadata}) {
+  Future<void> saveTaskMetadata({
+    required Metadata metadata,
+    required CollectionReference collectionRef,
+  }) {
     // TODO: implement saveMetadata
     throw UnimplementedError();
   }
 
   /// Add data about task items.
-  @override
   Future<void> saveTaskItem() {
     // TODO: implement saveItemData
     throw UnimplementedError();
