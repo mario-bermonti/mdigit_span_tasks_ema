796dd9c0f2f887d7602aae84eb1d3e26753eedf2
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-12-19 09:52:58 -0400

    refactor: Remove data source interface

--- lib/src/ema_db/data_source.dart
+++
@@ -1,7 +0,0 @@
-import 'models/metadata/metadata.dart';
-
-/// Provides an interface that standardizes data sources.
-abstract class DataSource {
-  Future<void> saveTaskMetadata({required Metadata metadata});
-  Future<void> saveTaskItem();
-}
