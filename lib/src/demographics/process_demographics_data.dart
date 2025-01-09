db86b8cf942c607d961517f1f80bb03d3a712016
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2025-01-09 15:30:17 -0400

    main(ema): Remove deprecated code

--- lib/src/demographics/process_demographics_data.dart
+++ lib/src/demographics/process_demographics_data.dart
@@ -2,7 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
 import 'package:mdigit_span_tasks_ema/src/auth/participant.dart';
 import 'package:mdigit_span_tasks_ema/src/ema_db/datasources/firebase_datasource.dart';
-import 'package:mdigit_span_tasks_ema/src/ema_db/ema_data.dart';
 import 'package:mdigit_span_tasks_ema/src/ema_db/study_task/models/metadata/metadata.dart';
 import 'package:mdigit_span_tasks_ema/src/ema_db/study_task/models/survey/survey.dart';
 import 'package:mdigit_span_tasks_ema/src/ema_db/study_task/models/survey/survey_item.dart';
