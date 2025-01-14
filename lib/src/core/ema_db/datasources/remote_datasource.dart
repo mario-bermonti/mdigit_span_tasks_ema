8650f649f0644b5e5feee6222406f804ebfedce6
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2025-01-14 08:59:39 -0400

    doc(ema_db): Add docstring to remote data source

--- lib/src/core/ema_db/datasources/remote_datasource.dart
+++ lib/src/core/ema_db/datasources/remote_datasource.dart
@@ -1,5 +1,6 @@
 import 'package:mdigit_span_tasks_ema/src/core/ema_db/models/ema_model.dart';
 
+/// Declares the contract that all remote data sources must implement.
 abstract class RemoteDataSource {
   /// Saves [EMAModel] to the data source.
   Future<void> saveEMAModel({
