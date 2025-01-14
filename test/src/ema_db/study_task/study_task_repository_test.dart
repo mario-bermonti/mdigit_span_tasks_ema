2a5427dfd2b164aad9c4080758ae790baa1b74dd
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2025-01-14 10:12:15 -0400

    Squash with "Improve organization of test cases"

--- test/src/ema_db/study_task/study_task_repository_test.dart
+++ test/src/ema_db/study_task/study_task_repository_test.dart
@@ -7,7 +7,7 @@ import 'package:mdigit_span_tasks_ema/src/core/ema_db/study_task/models/survey/s
 import 'package:mdigit_span_tasks_ema/src/core/ema_db/study_task/models/survey/survey_item.dart';
 import 'package:mdigit_span_tasks_ema/src/core/ema_db/study_task/study_task_repository.dart';
 
-import '../test_data/general.dart';
+import '../test_data/remote_datasource.dart';
 import '../test_data/metadata.dart';
 import '../test_data/survey.dart';
 
