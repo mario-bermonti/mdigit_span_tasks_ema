92c87891be5d3869a6ca5dc9d11e8e6db5003f6a
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2025-01-16 11:32:26 -0400

    feat(ema_db): Support saving participant locally

--- lib/src/core/ema_db/participant/participant_repository.dart
+++ lib/src/core/ema_db/participant/participant_repository.dart
@@ -1,20 +1,30 @@
+import 'package:mdigit_span_tasks_ema/src/core/ema_db/datasources/local_datasource.dart';
 import 'package:mdigit_span_tasks_ema/src/core/ema_db/datasources/remote_datasource.dart';
 import 'package:mdigit_span_tasks_ema/src/core/ema_db/participant/models/participant.dart';
 
 class ParticipantRepository {
   final RemoteDataSource _remoteDataSource;
+  final LocalDataSource _localDataSource;
 
   ParticipantRepository({
     required RemoteDataSource remoteDataSource,
-  }) : _remoteDataSource = remoteDataSource;
+    required LocalDataSource localDataSource,
+  })  : _remoteDataSource = remoteDataSource,
+        _localDataSource = localDataSource;
 
   Future<void> save({
     required Participant participant,
     required String pathRemoteDB,
+    required String pathLocalDB,
   }) async {
     await _remoteDataSource.saveEMAModel(
       emaModel: participant,
       path: pathRemoteDB,
+    );
+
+    await _localDataSource.saveEMAModel(
+      emaModel: participant,
+      path: pathLocalDB,
     );
   }
 }
