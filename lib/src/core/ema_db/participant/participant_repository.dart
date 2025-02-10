2c502f6fbef4227bffa0caae38e49ac7a46f6111
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2025-02-10 10:37:17 -0400

    doc(ema_db): Add docstring to participant repo

--- lib/src/core/ema_db/participant/participant_repository.dart
+++ lib/src/core/ema_db/participant/participant_repository.dart
@@ -2,6 +2,7 @@ import 'package:mdigit_span_tasks_ema/src/core/ema_db/datasources/local_datasour
 import 'package:mdigit_span_tasks_ema/src/core/ema_db/datasources/remote_datasource.dart';
 import 'package:mdigit_span_tasks_ema/src/core/ema_db/participant/models/participant.dart';
 
+/// Provides a simple interface for managing [Participant] metadata.
 class ParticipantRepository {
   final RemoteDataSource _remoteDataSource;
   final LocalDataSource _localDataSource;
