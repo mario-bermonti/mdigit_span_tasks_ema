ea22f35023747da00826128c533d24128fe28a97
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2025-02-25 20:10:18 -0400

    doc(ema_db): Add docstring to study progress step

--- lib/src/core/ema_db/progress/models/study_progress_step.dart
+++ lib/src/core/ema_db/progress/models/study_progress_step.dart
@@ -5,11 +5,18 @@ import 'package:mdigit_span_tasks_ema/src/core/ema_db/progress/models/status.dar
 part 'study_progress_step.freezed.dart';
 part 'study_progress_step.g.dart';
 
+/// Represents a single study step.
+///
+/// It is used as the basic unit to track participant's progress in the study.
 @freezed
 class StudyProgressStep extends EMAModel with _$StudyProgressStep {
   const factory StudyProgressStep({
     required String participantId,
+
+    /// Unique identifier of the study step
     required String stepId,
+
+    /// Status of the step. The steps are defined by [Status].
     required Status? status,
     required DateTime? lastUpdatedDateTime,
     String? stepDescription,
