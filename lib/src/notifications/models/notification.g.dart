a451227e24c2e4dd7dab2a3e75f29c9393b247a8
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2025-02-27 11:59:39 -0400

    feat: Define notification model for handlers

---
+++ lib/src/notifications/models/notification.g.dart
@@ -0,0 +1,31 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'notification.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$NotificationImpl _$$NotificationImplFromJson(Map<String, dynamic> json) =>
+    _$NotificationImpl(
+      notificationId: json['notificationId'] as String,
+      notificationType: json['notificationType'] as String?,
+      title: json['title'] as String?,
+      body: json['body'] as String?,
+      timeSent: json['timeSent'] == null
+          ? null
+          : DateTime.parse(json['timeSent'] as String),
+      timeTapped: json['timeTapped'] == null
+          ? null
+          : DateTime.parse(json['timeTapped'] as String),
+    );
+
+Map<String, dynamic> _$$NotificationImplToJson(_$NotificationImpl instance) =>
+    <String, dynamic>{
+      'notificationId': instance.notificationId,
+      'notificationType': instance.notificationType,
+      'title': instance.title,
+      'body': instance.body,
+      'timeSent': instance.timeSent?.toIso8601String(),
+      'timeTapped': instance.timeTapped?.toIso8601String(),
+    };
