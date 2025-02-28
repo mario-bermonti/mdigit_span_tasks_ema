7b191d09dc351619ad1ab385df1838cd66bb55e7
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2025-02-28 11:07:54 -0400

    feat(data): Define saving notification to db

--- lib/src/notifications/notifications_service.dart
+++ lib/src/notifications/notifications_service.dart
@@ -2,6 +2,9 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
 import 'package:mdigit_span_tasks_ema/src/core/ema_db/datasources/firebase_datasource.dart';
 import 'package:mdigit_span_tasks_ema/src/core/ema_db/notifications/notification_repository.dart';
+import 'package:mdigit_span_tasks_ema/src/core/ema_db/notifications/models/notification.dart'
+    as notification_model;
+import 'package:mdigit_span_tasks_ema/src/notifications/models/notification.dart';
 import 'package:mdigit_span_tasks_ema/src/notifications/notifications_manager.dart';
 
 class NotificationService extends GetxController {
@@ -24,5 +27,24 @@ class NotificationService extends GetxController {
       notificationRepository: NotificationRepository(
           remoteDataSource: FirebaseDataSource(db: FirebaseFirestore.instance)),
     );
+  }
+
+  /// Save notification to the remote db.
+  void saveMetadataToDB({required Notification notification}) {
+    final notification_model.Notification notificationModel =
+        notification_model.Notification(
+      participantId: _participantId,
+      notificationId: notification.notificationId,
+      notificationType: notification.notificationType,
+      title: notification.title,
+      body: notification.body,
+      timeSent: notification.timeSent,
+      timeTapped: notification.timeTapped,
+    );
+
+    final String pathRemoteDB =
+        'notifications/$_participantId/${notification.notificationId}';
+    _notificationRepository.save(
+        notification: notificationModel, pathRemoteDB: pathRemoteDB);
   }
 }
