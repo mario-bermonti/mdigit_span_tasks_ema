9bf9327325f84e9e385a93144e8e3eda80faf13c
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2025-04-03 12:36:24 -0400

    feat(notifications): Define service for notifications permissions
    feat(notifications): Define service to save notifications
    
    feat(notifications): Define service to save notifications
    
    feat(permission): Define base notifications permissions service
    
    fix(notifications): Fix base notifications permissions service
    
    feat(notifications): Define saving notifications permission using service
    
    fix(permissions): Add description to notifications permission
    
    fix(notifications): Fix remote db path for notifications permissions
    
    test(notifications): Test saving notifications permissions to remote db
    
    feat(notifications): Test to save notifications to local db
    
    improve(notifications): Extract permission id
    
    feat(notifications): Wrap save notifications with service

---
+++ lib/src/notifications/data/notifications_permission_service.dart
@@ -0,0 +1,76 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:mdigit_span_tasks_ema/src/core/ema_db/datasources/firebase_datasource.dart';
+import 'package:mdigit_span_tasks_ema/src/core/ema_db/datasources/getx_datasource.dart';
+import 'package:mdigit_span_tasks_ema/src/core/ema_db/permissions/models/permission.dart';
+import 'package:mdigit_span_tasks_ema/src/core/ema_db/permissions/models/status.dart';
+import 'package:mdigit_span_tasks_ema/src/core/ema_db/permissions/permission_repository.dart';
+
+/// Service that handles the permissions metadata.
+///
+/// It wraps the data repository to manage the metadata.
+class NotificationsPermissionService {
+  final PermissionRepository _permissionRepository;
+  final String permissionId = 'notifications';
+  final String _participantId;
+
+  NotificationsPermissionService({
+    required PermissionRepository permissionRepository,
+    required String participantId,
+  })  : _permissionRepository = permissionRepository,
+        _participantId = participantId;
+
+  /// Convenience method that initializes the service with all its dependencies.
+  ///
+  /// It serves as an informal named constructor.
+  static NotificationsPermissionService init({
+    required String participantId,
+  }) {
+    final FirebaseDataSource firebaseDataSource =
+        FirebaseDataSource(db: FirebaseFirestore.instance);
+    final GetxDataSource getxDataSource = GetxDataSource(db: GetStorage());
+    final PermissionRepository permissionRepository = PermissionRepository(
+      remoteDataSource: firebaseDataSource,
+      localDataSource: getxDataSource,
+    );
+
+    return NotificationsPermissionService(
+      permissionRepository: permissionRepository,
+      participantId: participantId,
+    );
+  }
+
+  /// Save notifications permission to the remote and local dbs.
+  Future<void> save() async {
+    final Permission permission = Permission(
+      participantId: _participantId,
+      permissionId: permissionId,
+      permissionDescription: 'Notifications permission',
+      dateTimeChanged: DateTime.now(),
+      status: Status.accepted,
+    );
+
+    final String pathRemoteDB =
+        'permissions/$_participantId/${permission.permissionId}';
+    const String pathLocalDB = 'notificationsPermissions';
+
+    await _permissionRepository.save(
+      permission: permission,
+      pathRemoteDB: pathRemoteDB,
+      pathLocalDB: pathLocalDB,
+    );
+  }
+
+  /// Fetches the latest notifications permission from dbs.
+  Future<Permission?> getLatest() async {
+    final String pathRemoteDB = 'permissions/$_participantId/$permissionId';
+    const String pathLocalDB = 'notificationsPermissions';
+
+    final Permission? permission = await _permissionRepository.getLatest(
+      pathRemoteDB: pathRemoteDB,
+      pathLocalDB: pathLocalDB,
+    );
+
+    return permission;
+  }
+}
