ac9140d6c57d9a52d0d3e121d3cc49939c2b551d
José L. Delgado Rosario <jlu0398@gmail.com>
2025-04-03 16:03:18 -0400

    improve: Add instructions to ema screen

--- lib/src/ema/ema_screen.dart
+++ lib/src/ema/ema_screen.dart
@@ -10,8 +10,25 @@ class EMAScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       appBar: CustomAppBar(),
-      body: Center(
-        child: EMAButton(),
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text("Vamos a continuar con las tareas que hemos estado practicando.",
+            style: TextStyle(fontSize: 30),
+            textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 40),
+            Text("Lee bien las instrucciones antes de comenzar porque el orden de las actividades cambia.",
+            style: TextStyle(fontSize: 30),
+            textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 60),
+            EMAButton(),
+          ],
+        ),
       ),
     );
   }
