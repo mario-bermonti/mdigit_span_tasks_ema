c1ca93e53f0d9318420418a8c2875b4db7801be9
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2025-04-09 16:59:08 -0400

    feat(notif): Use permissions to present permission screen

--- lib/src/landing/landing_controller.dart
+++ lib/src/landing/landing_controller.dart
@@ -1,7 +1,11 @@
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:mdigit_span_tasks_ema/src/core/ema_db/permissions/models/permission.dart';
+import 'package:mdigit_span_tasks_ema/src/core/ema_db/permissions/models/status.dart'
+    as permission_status;
 import 'package:mdigit_span_tasks_ema/src/core/ema_db/progress/models/study_progress_step.dart';
 import 'package:mdigit_span_tasks_ema/src/core/ema_db/progress/models/status.dart';
+import 'package:mdigit_span_tasks_ema/src/notifications/data/notifications_permission_service.dart';
 import 'package:mdigit_span_tasks_ema/src/notifications/notifications_service.dart';
 import 'package:mdigit_span_tasks_ema/src/study_progress/study_progress_service.dart';
 
@@ -31,14 +35,17 @@ class LandingController extends GetxController {
     );
     final bool demographicsSurveyCompleted =
         demographicsSurveyStep?.status == Status.completed;
-    final StudyProgressStep? notificationsStep = await studyProgressService.get(
-      participantId: participant.id,
-      stepId: 'remoteNotificationStep',
-    );
+
+    final NotificationsPermissionService notificationsPermissionService =
+        NotificationsPermissionService.init(participantId: participant.id);
+
+    final Permission? notificationsPermission =
+        await notificationsPermissionService.getLatest();
 
     if (!consentCompleted) {
       nextScreen = 'consent';
-    } else if (notificationsStep?.status != Status.completed) {
+    } else if (notificationsPermission?.status !=
+        permission_status.Status.accepted) {
       nextScreen = 'notificationsPermission';
     } else if (notificationService.notificationWhileOnTerminated != null) {
       nextScreen = 'emaScreen';
