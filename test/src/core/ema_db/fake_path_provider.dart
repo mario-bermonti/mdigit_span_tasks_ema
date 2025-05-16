c394b61aacbcd8a3fbf102aa5f81f64c2b8c1ae6
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2025-05-16 13:37:31 -0400

    fix: Run GetStorage tests in temp dir

--- test/src/core/ema_db/fake_path_provider.dart
+++ test/src/core/ema_db/fake_path_provider.dart
@@ -1,9 +1,13 @@
+import 'dart:io';
+import 'package:path/path.dart' as p;
+
 import 'package:path_provider_platform_interface/path_provider_platform_interface.dart';
 
 /// This fake provider is needed to mock the path provider in tests that rely on GetStorage.
 class FakePathProviderPlatform extends PathProviderPlatform {
   @override
   Future<String> getApplicationDocumentsPath() async {
-    return 'app_documents';
+    final Directory tempDir = await Directory.systemTemp.createTemp();
+    return p.join(tempDir.path, 'app_documents');
   }
 }
