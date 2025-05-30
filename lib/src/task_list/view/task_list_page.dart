1baf8b63f22d6c91c53c34b838145170b4bd2a7b
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2025-05-30 15:29:31 -0400

    Display pedometer data in task task list
    We do this to facilitate debugging.

--- lib/src/task_list/view/task_list_page.dart
+++ lib/src/task_list/view/task_list_page.dart
@@ -1,21 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:mdigits/src/app_bar/app_bar.dart';
+import 'package:mdigits/src/core/sensing/pedometer.dart/pedometer_service.dart';
 import 'task_buttons.dart';
 
 class TaskListPage extends StatelessWidget {
-  const TaskListPage({super.key});
+  TaskListPage({super.key});
+  final PedometerService pedometer = Get.find();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const CustomAppBar(),
-      body: const Center(
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            DSFButton(),
-            SizedBox(height: 10),
-            DSBButton(),
+            const DSFButton(),
+            const SizedBox(height: 10),
+            const DSBButton(),
+            StreamBuilder(
+              stream: pedometer.stepCountStream,
+              initialData: 'initial Data',
+              builder: (BuildContext context, AsyncSnapshot snapshot) {
+                if (snapshot.hasData) {
+                  return Text(
+                    'Steps: ${snapshot.data}',
+                    style: const TextStyle(fontSize: 20),
+                  );
+                } else {
+                  return const Text(
+                    'Some issue occurred!',
+                    style: TextStyle(fontSize: 20),
+                  );
+                }
+              },
+            ),
+            StreamBuilder(
+              stream: pedometer.pedestrianStatusStream,
+              initialData: 'initial Data',
+              builder: (BuildContext context, AsyncSnapshot snapshot) {
+                if (snapshot.hasData) {
+                  return Text(
+                    'Pedestrian Status: ${snapshot.data}',
+                    style: const TextStyle(fontSize: 20),
+                  );
+                } else {
+                  return const Text(
+                    'Some issue occurred!',
+                    style: TextStyle(fontSize: 20),
+                  );
+                }
+              },
+            ),
           ],
         ),
       ),
