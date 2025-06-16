00f5dca51c764786f50581b5b47297fa667f6267
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2025-06-16 12:31:18 -0400

    feat: Define app permissions repo
    feat: Define apps permissions repo
    
    improve: Define named constructor for permissions
    
    fix: Fix permissions repo names and imports
    
    feat: Add tests for permissions Repo

---
+++ lib/src/core/permissions/permissions_repository.dart
@@ -0,0 +1,71 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:mdigits/src/core/ema_db/datasources/firebase_datasource.dart';
+import 'package:mdigits/src/core/ema_db/datasources/getx_datasource.dart';
+import 'package:mdigits/src/core/ema_db/permissions/models/permission.dart';
+import 'package:mdigits/src/core/ema_db/permissions/permission_repository.dart'
+    as ema_db;
+
+/// Service that handles the permissions metadata.
+///
+/// It wraps the [ema_db] data repository to manage the metadata.
+class PermissionRepository {
+  final ema_db.PermissionRepository _permissionRepository;
+  final String _participantId;
+
+  PermissionRepository({
+    required ema_db.PermissionRepository permissionRepository,
+    required String participantId,
+  })  : _permissionRepository = permissionRepository,
+        _participantId = participantId;
+
+  /// Initializes the service with all its dependencies.
+  PermissionRepository.init({
+    required String participantId,
+  })  : _permissionRepository = ema_db.PermissionRepository(
+          remoteDataSource: FirebaseDataSource(db: FirebaseFirestore.instance),
+          localDataSource: GetxDataSource(db: GetStorage()),
+        ),
+        _participantId = participantId;
+
+  /// Save permission to the remote and local dbs.
+  Future<void> save({required Permission permission}) async {
+    final String pathRemoteDB =
+        'permissions/$_participantId/${permission.permissionId}';
+    String pathLocalDB = '${permission.permissionId}Permission';
+
+    await _permissionRepository.save(
+      permission: permission,
+      pathRemoteDB: pathRemoteDB,
+      pathLocalDB: pathLocalDB,
+    );
+  }
+
+  /// Fetches the latest permission data for the [permissionId] from dbs.
+  Future<Permission?> getLatest({required String permissionId}) async {
+    final String pathRemoteDB = 'permissions/$_participantId/$permissionId';
+    String pathLocalDB = '${permissionId}Permission';
+
+    final Permission? permission = await _permissionRepository.getLatest(
+      pathRemoteDB: pathRemoteDB,
+      pathLocalDB: pathLocalDB,
+    );
+
+    return permission;
+  }
+
+  /// Saves permission to the db if the status has changed since the most recent.
+  ///
+  /// It will do nothing if there is no permission in the db or if
+  /// the status has not changed.
+  Future<void> updateIfNecessary({required Permission permission}) async {
+    final Permission? latestPermission =
+        await getLatest(permissionId: permission.permissionId);
+    if (latestPermission == null) {
+      return;
+    } else if (latestPermission.status == permission.status) {
+      return;
+    }
+    await save(permission: permission);
+  }
+}
---
+++ test/src/core/permissions/permissions_repository_test.dart
@@ -0,0 +1,195 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:mdigits/src/core/ema_db/permissions/models/permission.dart';
+import 'package:mdigits/src/core/permissions/permissions_repository.dart';
+import 'package:mdigits/src/core/ema_db/datasources/firebase_datasource.dart';
+import 'package:mdigits/src/core/ema_db/datasources/getx_datasource.dart';
+import 'package:mdigits/src/core/ema_db/permissions/permission_repository.dart'
+    as ema_db;
+import 'package:path_provider_platform_interface/path_provider_platform_interface.dart';
+
+import '../ema_db/fake_path_provider.dart';
+import 'permissions_repo_test_cases.dart';
+
+void main() {
+  late FakeFirebaseFirestore remoteDB;
+  late GetStorage localDB;
+  late PermissionRepository repo;
+  // late PermissionsRepository repo;
+
+  setUp(() async {
+    TestWidgetsFlutterBinding.ensureInitialized();
+    PathProviderPlatform.instance = FakePathProviderPlatform();
+    await GetStorage.init();
+    localDB = GetStorage();
+
+    remoteDB = FakeFirebaseFirestore();
+    final ema_db.PermissionRepository permissionRepository =
+        ema_db.PermissionRepository(
+      remoteDataSource: FirebaseDataSource(db: remoteDB),
+      localDataSource: GetxDataSource(db: localDB),
+    );
+
+    repo = PermissionRepository(
+        permissionRepository: permissionRepository,
+        participantId: testAcceptedPermission.participantId);
+  });
+
+  tearDown(() {
+    remoteDB.clearPersistence();
+    localDB.erase();
+  });
+
+  group('PermissionsRepository.save', () {
+    test(
+      "Saves an accepted permission with correct data to the remote db.",
+      () async {
+        await repo.save(permission: testAcceptedPermission);
+
+        final QuerySnapshot<Map<String, dynamic>> snapshot =
+            await remoteDB.collection(testPathRemoteDB).get();
+        final Permission actualPermission =
+            Permission.fromJson(snapshot.docs.first.data());
+
+        expect(actualPermission, testAcceptedPermission);
+      },
+    );
+    test(
+      "Saves an accepted permission with correct data to the local db.",
+      () async {
+        await repo.save(permission: testAcceptedPermission);
+
+        final Map<String, dynamic> permissionJson =
+            localDB.read(testPathLocalDB);
+        final Permission actualPermission = Permission.fromJson(permissionJson);
+
+        expect(actualPermission, testAcceptedPermission);
+      },
+    );
+  });
+  group('PermissionsRepository.getLatest', () {
+    test(
+      "Returns valid permission if it only exists in remote.",
+      () async {
+        remoteDB
+            .collection(testPathRemoteDB)
+            .add(testAcceptedPermission.toJson());
+
+        final Permission? actualPermission = await repo.getLatest(
+            permissionId: testAcceptedPermission.permissionId);
+
+        expect(actualPermission, testAcceptedPermission);
+      },
+    );
+    test(
+      "Returns valid permission if it only exists in local.",
+      () async {
+        await localDB.write(testPathLocalDB, testAcceptedPermission.toJson());
+
+        final Permission? actualPermission = await repo.getLatest(
+            permissionId: testAcceptedPermission.permissionId);
+
+        expect(actualPermission, testAcceptedPermission);
+      },
+    );
+  });
+  group('PermissionsRepository.updateIfNecessary', () {
+    test(
+      "Given that there are no previous permissions in remote db, does nothing.",
+      () async {
+        await repo.updateIfNecessary(permission: testAcceptedPermission);
+
+        final QuerySnapshot<Map<String, dynamic>> snapshot =
+            await remoteDB.collection(testPathRemoteDB).get();
+        expect(snapshot.docs.isEmpty, true);
+      },
+    );
+    test(
+      "Given that the new and the permission in remote db are both accepted, "
+      "does nothing.",
+      () async {
+        await remoteDB
+            .collection(testPathRemoteDB)
+            .add(testAcceptedPermission.toJson());
+
+        await repo.updateIfNecessary(permission: testAcceptedPermission);
+
+        final QuerySnapshot<Map<String, dynamic>> snapshot =
+            await remoteDB.collection(testPathRemoteDB).get();
+        expect(snapshot.docs.length, 1);
+      },
+    );
+    test(
+      "Given that the new and the permission in remote db are both denied, "
+      "does nothing.",
+      () async {
+        await remoteDB
+            .collection(testPathRemoteDB)
+            .add(testDeniedPermission.toJson());
+
+        await repo.updateIfNecessary(permission: testDeniedPermission);
+
+        final QuerySnapshot<Map<String, dynamic>> snapshot =
+            await remoteDB.collection(testPathRemoteDB).get();
+
+        expect(snapshot.docs.length, 1);
+      },
+    );
+    test(
+      "Given that permissions have changed in remote db, "
+      "save the new permission object to db.",
+      () async {
+        await remoteDB
+            .collection(testPathRemoteDB)
+            .add(testAcceptedPermission.toJson());
+
+        await repo.updateIfNecessary(permission: testDeniedPermission);
+
+        final QuerySnapshot<Map<String, dynamic>> snapshot =
+            await remoteDB.collection(testPathRemoteDB).get();
+
+        expect(snapshot.docs.length, 2);
+      },
+    );
+    test(
+      "Given that there are no previous permissions in local db, does nothing.",
+      () async {
+        await repo.updateIfNecessary(permission: testAcceptedPermission);
+
+        final Map<String, dynamic>? actualPermission =
+            localDB.read(testPathLocalDB);
+        expect(actualPermission, null);
+      },
+    );
+    test(
+      "Given that permissions have not changed in local db, "
+      "does nothing and keeps on being only one permission in db.",
+      () async {
+        await localDB.write(testPathLocalDB, testAcceptedPermission.toJson());
+
+        await repo.updateIfNecessary(permission: testAcceptedPermission);
+
+        final Map<String, dynamic> actualPermission =
+            localDB.read(testPathLocalDB);
+        expect(Permission.fromJson(actualPermission), testAcceptedPermission);
+      },
+    );
+    test(
+      "Given that permissions have changed in local db, "
+      "save the new permission object to db.",
+      () async {
+        await localDB.write(testPathLocalDB, testAcceptedPermission.toJson());
+
+        await repo.updateIfNecessary(permission: testDeniedPermission);
+
+        final Map<String, dynamic> actualPermission =
+            localDB.read(testPathLocalDB);
+
+        expect(Permission.fromJson(actualPermission),
+            isNot(testAcceptedPermission));
+      },
+    );
+  });
+}
